7c59fc904335f842cda3a7a39dda1fe9d4917fb3
Logan Yu <yu.logan@gmail.com>
2019-04-11 03:34:16 -0400

    add 236_lowest_common_ancestor_of_a_binary_tree

---
+++ 236_lowest_common_ancestor_of_a_binary_tree.rb
@@ -0,0 +1,66 @@
+=begin
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+
+
+ 
+
+Example 1:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+Example 2:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+ 
+
+Note:
+
+All of the nodes' values will be unique.
+p and q are different and both values will exist in the binary tree.
+=end
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {TreeNode}
+def lowest_common_ancestor(root, p, q)
+    ans = []
+    recurse_tree(root, p, q, ans)
+    
+    ans[0]
+end
+
+def recurse_tree(node, p, q, ans)
+    if node.nil?
+        return 0
+    end
+    left = recurse_tree(node.left, p, q, ans)
+    right = recurse_tree(node.right, p, q, ans)
+    if node == p || node == q
+        mid = 1
+    else
+        mid = 0
+    end
+    
+    if mid + left + right >= 2
+        ans << node
+    end
+    
+    return [mid, left, right].max
+end
