570b0735b1b725abb40804a95f2da3a064618fc3
Logan Yu <yu.logan@gmail.com>
2019-04-11 11:46:09 -0400

    add 647_palindromic_substrings

---
+++ 647_palindromic_substrings.rb
@@ -0,0 +1,41 @@
+=begin
+Given a string, your task is to count how many palindromic substrings in this string.
+
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+Example 1:
+
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+ 
+
+Example 2:
+
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+Note:
+
+The input string length won't exceed 1000.
+=end
+
+# @param {String} s
+# @return {Integer}
+def count_substrings(s)
+    n = s.length
+    ans = 0
+    0.upto(2*n - 1).each do |center|
+        left = center / 2
+        right = left + center % 2
+        while left >= 0 && right < n && s[left] == s[right]
+            ans += 1
+            left -= 1
+            right += 1
+        end
+    end
+    
+    return ans
+end
