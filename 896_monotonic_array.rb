21df078a1d96a4decdf63acf1e6ff1db19f2488b
Logan Yu <yu.logan@gmail.com>
2019-04-17 14:08:53 -0400

    add 896_monotonic_array

---
+++ 896_monotonic_array.rb
@@ -0,0 +1,22 @@
+=begin
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+
+Return true if and only if the given array A is monotonic.
+=end
+
+# @param {Integer[]} a
+# @return {Boolean}
+def is_monotonic(a)
+    increasing = decreasing = true
+    (0...a.length - 1).each do |i|
+        if a[i] > a[i+1]
+            increasing = false
+        elsif a[i] < a[i+1]
+            decreasing = false
+        end
+    end
+    
+    return increasing || decreasing
+end
