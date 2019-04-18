d6950771940147eecb6627c9055f8a530711a26f
Logan Yu <yu.logan@gmail.com>
2019-04-18 12:14:23 -0400

    add 426_convert_binary_search_tree_to_sorted_doubly_linked_list.py

---
+++ 426_convert_binary_search_tree_to_sorted_doubly_linked_list.py
@@ -0,0 +1,42 @@
+"""
+Convert a BST to a sorted circular doubly-linked list in-place. Think of the left and right pointers as synonymous to the previous and next pointers in a doubly-linked list.
+
+Let's take the following BST as an example, it may help you understand the problem better:
+
+We want to transform this BST into a circular doubly linked list. Each node in a doubly linked list has a predecessor and successor. For a circular doubly linked list, the predecessor of the first element is the last element, and the successor of the last element is the first element.
+
+The figure below shows the circular doubly linked list for the BST above. The "head" symbol means the node it points to is the smallest element of the linked list.
+
+Specifically, we want to do the transformation in place. After the transformation, the left pointer of the tree node should point to its predecessor, and the right pointer should point to its successor. We should return the pointer to the first element of the linked list.
+
+The figure below shows the transformed BST. The solid line indicates the successor relationship, while the dashed line means the predecessor relationship.
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        def helper(node):
+            nonlocal first, last
+            if node:
+                helper(node.left)
+                if last:
+                    node.left = last
+                    last.right = node
+                    last = node
+                else:
+                    last = first = node
+                helper(node.right)
+        if not root:
+            return None
+        first, last = None, None
+        helper(root)
+        first.left = last
+        last.right = first
+        return first
