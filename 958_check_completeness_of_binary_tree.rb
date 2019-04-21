94af1e2dbca638a7443cbf7af5301dcafa26c34c
Logan Yu <yu.logan@gmail.com>
2019-04-21 14:20:24 -0400

    add 958_check_completeness_of_binary_tree.rb

---
+++ 958_check_completeness_of_binary_tree.rb
@@ -0,0 +1,57 @@
+=begin
+Given a binary tree, determine if it is a complete binary tree.
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+ 
+
+Example 1:
+
+
+
+Input: [1,2,3,4,5,6]
+Output: true
+Explanation: Every level before the last is full (ie. levels with node-values {1} and {2, 3}), and all nodes in the last level ({4, 5, 6}) are as far left as possible.
+Example 2:
+
+
+
+Input: [1,2,3,4,5,null,7]
+Output: false
+Explanation: The node with value 7 isn't as far left as possible.
+ 
+Note:
+
+The tree will have between 1 and 100 nodes.
+=end
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Boolean}
+def is_complete_tree(root)
+    queue = [root]
+    nil_node_found = false
+    while !queue.empty?
+        curr = queue.shift
+        if curr.nil?
+            nil_node_found = true
+        else
+            if nil_node_found
+                return false
+            end
+            queue.push(curr.left)
+            queue.push(curr.right)
+        end
+    end
+    
+    return true
+end
