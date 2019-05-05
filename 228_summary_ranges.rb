ee849f5f501034050ff78c3d1e4a9c59e235e4a1
Logan Yu <yu.logan@gmail.com>
2019-05-05 16:42:03 +0800

    add 228_summary_ranges.rb

---
+++ 228_summary_ranges.rb
@@ -0,0 +1,44 @@
+=begin
+Given a sorted integer array without duplicates, return the summary of its ranges.
+
+Example 1:
+
+Input:  [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: 0,1,2 form a continuous range; 4,5 form a continuous range.
+Example 2:
+
+Input:  [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: 2,3,4 form a continuous range; 8,9 form a continuous range.
+=end
+
+# @param {Integer[]} nums
+# @return {String[]}
+def summary_ranges(nums)
+    summary = []
+    prev_num = nil
+    range_start = nil
+    nums.each do |num|
+        if range_start.nil?
+            range_start = num
+        elsif prev_num + 1 != num
+            add_range_to_summary(range_start, prev_num, summary)
+            range_start = num
+        end
+        prev_num = num
+    end
+    if !range_start.nil?
+        add_range_to_summary(range_start, prev_num, summary)
+    end
+
+    return summary
+end
+
+def add_range_to_summary(range_start, prev_num, summary)
+    if range_start == prev_num
+        summary << "#{range_start}"
+    else
+        summary << "#{range_start}->#{prev_num}"
+    end
+end
