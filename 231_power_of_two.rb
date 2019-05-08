c1aedbc413780ad7f92ce1065b6fb24fa454cae1
Logan Yu <yu.logan@gmail.com>
2019-05-08 18:54:05 +0800

    add 231_power_of_two.rb

---
+++ 231_power_of_two.rb
@@ -0,0 +1,36 @@
+# @param {Integer} n
+# @return {Boolean}
+# O(log n)
+def is_power_of_two(n)
+    if n <= 0
+        return false
+    end
+    while n % 2 == 0
+        n /= 2
+    end
+    
+    n == 1
+end
+
+# bitwise solution. O(1)
+def is_power_of_two(n)
+    if n <= 0
+        return false;
+    end
+    
+    return (n&(n-1)) == 0
+end
+
+# math derivation O(1)
+def is_power_of_two(n)
+    if n <= 0
+        return false;
+    end
+    
+    return 2**30 % n == 0
+end
+
+# bitcount O(1)
+def is_power_of_two(n)
+  return n > 0 && n.to_s(2).count("1") == 1
+end
