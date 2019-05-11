44f233f2e0ed95abe5d34ec28bff6a68cf42d279
Logan Yu <yu.logan@gmail.com>
2019-05-11 09:00:19 +0800

    add 326_power_of_three.rb

---
+++ 326_power_of_three.rb
@@ -0,0 +1,18 @@
+# @param {Integer} n
+# @return {Boolean}
+def is_power_of_three(n)
+    # 3**19 is max int to power of 3
+    return n > 0 && 3**19 % n == 0
+end
+
+# recursive
+def is_power_of_three(n)
+    if n > 1
+        while n % 3 == 0
+            n /= 3
+        end
+    end
+    
+    return n == 1
+end
+
