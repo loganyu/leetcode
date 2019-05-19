5a9a52d63cafa736fffc53ccd4471567ef41608a
Logan Yu <yu.logan@gmail.com>
2019-05-19 10:35:05 +0800

    refactor 939_minimum_area_rectangle.rb

--- 939_minimum_area_rectangle.rb
+++ 939_minimum_area_rectangle.rb
@@ -33,29 +33,30 @@ end
 # @return {Integer}
 def min_area_rect(points)
     columns = {}
-    points.each do |x,y|
+    points.each do |x, y|
         columns[x] ||= []
         columns[x] << y
     end
+    
+    min = Float::INFINITY
     last_x = {}
-    ans = Float::INFINITY
-    columns.keys.sort.each do |x|
-        column = columns[x].sort
-        column.each_with_index do |y2, i|
-            (0...i).each do |j|
-                y1 = column[j]
-                if last_x[[y1, y2]]
-                    ans = [ans, (x - last_x[[y1,y2]]) * (y2 - y1)].min
+    columns.keys.sort.each do |x2|
+        column = columns[x2].sort
+        (0...column.length-1).each do |i|
+            (i+1...column.length).each do |j|
+                y1 = column[i]
+                y2 = column[j]
+                if last_x[[y1,y2]]
+                    x1 = last_x[[y1,y2]]
+                    area = (x2 - x1) * (y2 - y1)
+                    min = [min, area].min
                 end
-                last_x[[y1,y2]] = x
+                last_x[[y1,y2]] = x2
             end
         end
     end
-    if ans == Float::INFINITY
-        return 0
-    else
-        return ans
-    end
+    
+    return min == Float::INFINITY ? 0 : min
 end
 
 =begin
