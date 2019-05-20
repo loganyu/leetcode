e7bc576bcd7dee9a7d10fad42448e10701df4e3a
Logan Yu <yu.logan@gmail.com>
2019-05-20 12:23:36 +0800

    update 056_merge_intervals.rb with new input types

--- 056_merge_intervals.rb
+++ 056_merge_intervals.rb
@@ -1,38 +1,13 @@
-=begin
-Given a collection of intervals, merge all overlapping intervals.
-
-Example 1:
-
-Input: [[1,3],[2,6],[8,10],[15,18]]
-Output: [[1,6],[8,10],[15,18]]
-Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
-Example 2:
-
-Input: [[1,4],[4,5]]
-Output: [[1,5]]
-Explanation: Intervals [1,4] and [4,5] are considered overlapping.
-=end
-
-# Definition for an interval.
-# class Interval
-#     attr_accessor :start, :end
-#     def initialize(s=0, e=0)
-#         @start = s
-#         @end = e
-#     end
-# end
-
-# @param {Interval[]} intervals
-# @return {Interval[]}
+# @param {Integer[][]} intervals
+# @return {Integer[][]}
 def merge(intervals)
-    intervals.sort_by!{|interval| interval.start}
     sol = []
-    intervals.each do |interval|
-        if sol.empty? || sol.last.end < interval.start
+    intervals.sort_by{|s,e| s}.each do |interval|
+        if sol.empty? || sol[-1][1] < interval[0]
             sol << interval
         else
-            if sol.last.end < interval.end
-                sol.last.end = interval.end
+            if sol[-1][1] < interval[1]
+                sol[-1][1] = interval[1]
             end
         end
     end
