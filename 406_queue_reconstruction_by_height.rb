137969428fabb1ba25f4667685e50b074d68cfe7
Logan Yu <yu.logan@gmail.com>
2019-05-21 11:57:22 +0800

    add 406_queue_reconstruction_by_height.rb

---
+++ 406_queue_reconstruction_by_height.rb
@@ -0,0 +1,37 @@
+=begin
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+
+
+Example
+
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+=end
+
+# @param {Integer[][]} people
+# @return {Integer[][]}
+def reconstruct_queue(people)
+    map = {}
+    people.each do |h,k|
+        map[h] ||= []
+        map[h] << [h,k]
+    end
+    heights = map.keys.sort_by{|h| -h}
+    sol = []
+    
+    heights.each do |h|
+        group = map[h]
+        group.sort.each do |h,k|
+            sol.insert(k, [h,k])
+        end
+    end
+    
+    sol
+end
+
