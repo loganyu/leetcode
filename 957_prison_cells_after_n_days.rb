0bd3896a883cfa69882b08a4984b7d2b523abae4
Logan Yu <yu.logan@gmail.com>
2019-05-28 12:19:29 +0800

    add 957_prison_cells_after_n_days.rb

---
+++ 957_prison_cells_after_n_days.rb
@@ -0,0 +1,78 @@
+=begin
+There are 8 prison cells in a row, and each cell is either occupied or vacant.
+
+Each day, whether the cell is occupied or vacant changes according to the following rules:
+
+If a cell has two adjacent neighbors that are both occupied or both vacant, then the cell becomes occupied.
+Otherwise, it becomes vacant.
+(Note that because the prison is a row, the first and the last cells in the row can't have two adjacent neighbors.)
+
+We describe the current state of the prison in the following way: cells[i] == 1 if the i-th cell is occupied, else cells[i] == 0.
+
+Given the initial state of the prison, return the state of the prison after N days (and N such changes described above.)
+
+ 
+
+Example 1:
+
+Input: cells = [0,1,0,1,1,0,0,1], N = 7
+Output: [0,0,1,1,0,0,0,0]
+Explanation: 
+The following table summarizes the state of the prison on each day:
+Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
+Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
+Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
+Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
+Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
+Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
+Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
+Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
+
+Example 2:
+
+Input: cells = [1,0,0,1,0,0,1,0], N = 1000000000
+Output: [0,0,1,1,1,1,1,0]
+ 
+
+Note:
+
+cells.length == 8
+cells[i] is in {0, 1}
+1 <= N <= 10^9
+
+=end
+
+# @param {Integer[]} cells
+# @param {Integer} n
+# @return {Integer[]}
+def prison_after_n_days(cells, n)
+    seen = {}
+    while n > 0
+        if seen[cells]
+            n = n % (seen[cells] - n)
+        end
+        seen[cells] = n
+        if n >= 1
+            n -= 1
+            cells = next_day(cells)
+        end
+    end
+    
+    cells
+end
+
+def next_day(cells)
+    ans = []
+    (0...cells.length).each do |i|
+        if i == 0 || i == cells.length - 1
+            ans << 0
+        elsif cells[i-1] == cells[i+1]
+            ans << 1
+        else
+            ans << 0
+        end
+    end
+    
+    return ans
+end
+
