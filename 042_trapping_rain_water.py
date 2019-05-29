7438418422415f7734b6a58c080bdd5fbe3ae9b7
Logan Yu <yu.logan@gmail.com>
2019-05-29 13:24:24 +0800

    add 042_trapping_rain_water.py

---
+++ 042_trapping_rain_water.py
@@ -0,0 +1,31 @@
+'''
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+
+The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
+
+Example:
+
+Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+'''
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        if n == 0:
+            return 0
+        left_max = [None]*n
+        left_max[0] = height[0]
+        for i in range(1, n):
+            left_max[i] = max(left_max[i-1], height[i])
+        right_max = [None]*n
+        right_max[-1] = height[-1]
+        for j in reversed(range(0, n-1)):
+            right_max[j] = max(right_max[j+1], height[j])
+        ans = 0
+        for i in range(0,n):
+            ans += min(left_max[i], right_max[i]) - height[i]
+        
+        return ans
+
