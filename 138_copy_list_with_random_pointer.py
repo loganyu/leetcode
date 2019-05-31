c9284688e69cbee43da422beea434725f1548ade
Logan Yu <yu.logan@gmail.com>
2019-05-31 15:02:24 +0800

    update to python3 138_copy_list_with_random_pointer.py

--- 138_copy_list_with_random_pointer.py
+++ 138_copy_list_with_random_pointer.py
@@ -24,31 +24,23 @@ You must return the copy of the given head as a reference to the cloned list.
 
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val, next, random):
         self.val = val
         self.next = next
         self.random = random
 """
-class Solution(object):
+class Solution:
     def __init__(self):
         self.visitedHash = {}
-    
-    def copyRandomList(self, head):
-        """
-        :type head: Node
-        :rtype: Node
-        """
+    def copyRandomList(self, head: 'Node') -> 'Node':
         if not head:
             return head
-        
         if head in self.visitedHash:
             return self.visitedHash[head]
+        copy = Node(head.val, None, None)
+        self.visitedHash[head] = copy
+        copy.next = self.copyRandomList(head.next)
+        copy.random = self.copyRandomList(head.random)
         
-        node = Node(head.val, None, None)
-        self.visitedHash[head] = node
-        node.next = self.copyRandomList(head.next)
-        node.random = self.copyRandomList(head.random)
-        
-        return node
-        
+        return copy 
