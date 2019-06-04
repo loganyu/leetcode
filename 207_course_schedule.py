37134b5034cb7be41a2a516aeff9538dd6896eb3
Logan Yu <yu.logan@gmail.com>
2019-06-04 14:29:07 +0800

    add 207_course_schedule.py

---
+++ 207_course_schedule.py
@@ -0,0 +1,57 @@
+'''
+There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+Example 1:
+
+Input: 2, [[1,0]] 
+Output: true
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+
+Input: 2, [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+Note:
+
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+'''
+
+from collections import defaultdict, deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        degrees = {}
+        for course in range(numCourses):
+            degrees[course] = 0
+        
+        courses_by_prereq = defaultdict(list)
+        for course, prereq in prerequisites:
+            courses_by_prereq[prereq].append(course)
+            degrees[course] += 1
+            
+        
+        queue = deque()
+        for course, num_degrees in degrees.items():
+            if num_degrees == 0:
+                queue.append(course)
+        
+        completed = 0
+        while len(queue) > 0:
+            course = queue.popleft()
+            completed += 1
+            for next_course in courses_by_prereq[course]:
+                degrees[next_course] -= 1
+                if degrees[next_course] == 0:
+                    queue.append(next_course)
+        
+        return completed == numCourses
+            
+        
