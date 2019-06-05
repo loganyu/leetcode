dbb9abd974c6f9e813cb006620e386f0abd1f1e3
Logan Yu <yu.logan@gmail.com>
2019-06-05 15:31:37 +0800

    refactor 028_implement_strStr.py

--- 028_implement_strStr.py
+++ 028_implement_strStr.py
@@ -25,8 +25,7 @@ class Solution:
         if m == 0:
             return 0
         for i in range(n-m+1):
-            if haystack[i:i+m] == needle:
+            if haystack[i] == needle[0] and haystack[i:i+m] == needle:
                 return i
         
         return -1
-
