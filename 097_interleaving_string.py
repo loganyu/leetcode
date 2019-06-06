f76a9d87e040f6bc328f54a5eca3e71d640f291c
Logan Yu <yu.logan@gmail.com>
2019-06-06 15:36:57 +0800

    add 097_interleaving_string.py

---
+++ 097_interleaving_string.py
@@ -0,0 +1,31 @@
+'''
+Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+
+Example 1:
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true
+Example 2:
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+Output: false
+'''
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s3) != len(s1) + len(s2):
+            return False
+        dp = [[None for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
+        for i in range(len(s1)+1):
+            for j in range(len(s2)+1):
+                if i == 0 and j == 0:
+                    dp[i][j] = True
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[i+j-1]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i+j-1]
+                else:
+                    dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+
+        return dp[-1][-1]
+
