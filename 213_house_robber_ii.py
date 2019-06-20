3bf630cf34690cd812a0d6bd98f65219b9b61c4e
Logan Yu <yu.logan@gmail.com>
2019-06-20 15:12:56 -0400

    add 213_house_robber_ii.py

---
+++ 213_house_robber_ii.py
@@ -0,0 +1,39 @@
+'''
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+             because they are adjacent houses.
+Example 2:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+'''
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        
+        return max(self.rob_linear(nums[:n-1]), self.rob_linear(nums[1:]))
+    
+    def rob_linear(self, nums: List[int]) -> int:
+        prev_max = 0
+        curr_max = 0
+        for num in nums:
+            temp = curr_max
+            curr_max = max(curr_max, prev_max + num)
+            prev_max = temp
+            
+        return curr_max
+
