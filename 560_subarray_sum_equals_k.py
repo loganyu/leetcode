6359e8259ff0276da4ec21ba58ac4686f9136010
Logan Yu <yu.logan@gmail.com>
2019-06-23 10:59:34 -0400

    use defaultdict for 560_subarray_sum_equals_k.py

--- 560_subarray_sum_equals_k.py
+++ 560_subarray_sum_equals_k.py
@@ -9,18 +9,18 @@ The length of the array is in range [1, 20,000].
 The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
 '''
 
+from collections import defaultdict
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         k_count = 0
         s = 0
-        count_by_sum = {}
+        count_by_sum = defaultdict(int)
         count_by_sum[0] = 1
         for num in nums:
             s += num
             if s - k in count_by_sum:
                 k_count += count_by_sum[s - k]
-            if s not in count_by_sum:
-                count_by_sum[s] = 0
             count_by_sum[s] += 1
         
         return k_count
