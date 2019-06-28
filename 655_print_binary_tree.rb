b08fc17a0047f73f504bcbb72176bb93f1afdc30
Logan Yu <yu.logan@gmail.com>
2019-06-28 12:57:30 -0400

    add 655_print_binary_tree.rb

---
+++ 655_print_binary_tree.rb
@@ -0,0 +1,82 @@
+=begin
+Print a binary tree in an m*n 2D string array following these rules:
+
+The row number m should be equal to the height of the given binary tree.
+The column number n should always be an odd number.
+The root node's value (in string format) should be put in the exactly middle of the first row it can be put. The column and the row where the root node belongs will separate the rest space into two parts (left-bottom part and right-bottom part). You should print the left subtree in the left-bottom part and print the right subtree in the right-bottom part. The left-bottom part and the right-bottom part should have the same size. Even if one subtree is none while the other is not, you don't need to print anything for the none subtree but still need to leave the space as large as that for the other subtree. However, if two subtrees are none, then you don't need to leave space for both of them.
+Each unused space should contain an empty string "".
+Print the subtrees following the same rules.
+Example 1:
+Input:
+     1
+    /
+   2
+Output:
+[["", "1", ""],
+ ["2", "", ""]]
+Example 2:
+Input:
+     1
+    / \
+   2   3
+    \
+     4
+Output:
+[["", "", "", "1", "", "", ""],
+ ["", "2", "", "", "", "3", ""],
+ ["", "", "4", "", "", "", ""]]
+Example 3:
+Input:
+      1
+     / \
+    2   5
+   / 
+  3 
+ / 
+4 
+Output:
+
+[["",  "",  "", "",  "", "", "", "1", "",  "",  "",  "",  "", "", ""]
+ ["",  "",  "", "2", "", "", "", "",  "",  "",  "",  "5", "", "", ""]
+ ["",  "3", "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]
+ ["4", "",  "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]]
+Note: The height of binary tree is in the range of [1, 10].
+=end
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {String[][]}
+def print_tree(root)
+    height = get_height(root)
+    res = Array.new(height){Array.new(2**height - 1){""}}
+    fill(res, root, 0, 0, res[0].length)
+    
+    return res
+end
+
+def fill(res, root, i, l, r)
+    if root.nil?
+        return
+    end
+    
+    res[i][(l+r)/2] = root.val.to_s
+    fill(res, root.left, i+1, l, (l+r)/2)
+    fill(res, root.right, i+1, (l+r+1)/2, r)
+end
+
+def get_height(root)
+    if root.nil?
+        return 0
+    end
+    
+    return 1 + [get_height(root.left), get_height(root.right)].max
+end
+
