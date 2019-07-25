2afa06f6c51692f1a9ea0ab087807320b2037caa
Logan Yu <yu.logan@gmail.com>
2019-07-25 15:35:13 -0400

    add 700_search_in_a_binary_search_tree.rb

---
+++ 700_search_in_a_binary_search_tree.rb
@@ -0,0 +1,49 @@
+=begin
+Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+For example, 
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+And the value to search: 2
+You should return this subtree:
+
+      2     
+     / \   
+    1   3
+In the example above, if we want to search the value 5, since there is no node with value 5, we should return NULL.
+
+Note that an empty tree is represented by NULL, therefore you would see the expected output (serialized tree format) as [], not null.
+=end
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @param {Integer} val
+# @return {TreeNode}
+def search_bst(root, val)
+    while !root.nil?
+        if root.val == val
+            return root
+        elsif root.val < val
+            root = root.right
+        else
+            root = root.left
+        end
+    end
+    
+    return root
+end
+
