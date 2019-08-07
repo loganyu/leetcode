f0ea6aa75ae5f24b260fb5d988b187b3121c0a0c
Logan Yu <yu.logan@gmail.com>
2019-08-07 19:13:53 -0400

    add 230_kth_smallest_element_in_a_bst.rb

---
+++ 230_kth_smallest_element_in_a_bst.rb
@@ -0,0 +1,59 @@
+=begin
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Example 1:
+
+Input: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+Output: 1
+Example 2:
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+Output: 3
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+=end
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @param {Integer} k
+# @return {Integer}
+def kth_smallest(root, k)
+    stack = []
+    
+    while true
+        while root
+            stack.push(root)
+            root = root.left
+        end
+        root = stack.pop
+        k -= 1
+        if k == 0
+            return root.val
+        end
+        root = root.right
+    end
+end
+
