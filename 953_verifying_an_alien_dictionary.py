a7699e9e870e73a86bd5b29a36ff27f3fd0038d2
Logan Yu <yu.logan@gmail.com>
2019-10-28 23:05:09 -0400

    add 953_verifying_an_alien_dictionary.py

---
+++ 953_verifying_an_alien_dictionary.py
@@ -0,0 +1,51 @@
+"""
+In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
+
+ 
+
+Example 1:
+
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+Example 2:
+
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+ 
+
+Note:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 20
+order.length == 26
+All characters in words[i] and order are english lowercase letters.
+"""
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_index = {c: i for i, c in enumerate(order)}
+         
+        for i in range(len(words) - 1):
+            word1 = words[i]
+            word2 = words[i+1]
+            
+            for k in range(min(len(word1), len(word2))):
+                if word1[k] != word2[k]:
+                    if order_index[word1[k]] > order_index[word2[k]]:
+                        return False
+                    break
+            else:
+                if len(word1) > len(word2):
+                    return False
+        
+        return True
+                    
