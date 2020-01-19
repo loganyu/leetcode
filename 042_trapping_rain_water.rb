e8ad91f6238eb6f21cc3fcabf5ad41f2f34d6600
Logan Yu <logan.yu@cadre.com>
2020-01-19 17:28:05 -0600

    add one pass solution to 042_trapping_rain_water.rb

--- 042_trapping_rain_water.rb
+++ 042_trapping_rain_water.rb
@@ -34,3 +34,26 @@ def trap(height)
     
     ans
 end
+
+# one pass solution
+# @param {Integer[]} height
+# @return {Integer}
+def trap(height)
+    left, right = 0, height.length - 1
+    left_max = right_max = 0
+    ans = 0
+    while left < right
+        if height[left] < height[right]
+            left_max = [left_max, height[left]].max
+            ans += left_max - height[left]
+            left += 1
+        else
+            right_max = [right_max, height[right]].max
+            ans += right_max - height[right]
+            right -= 1
+        end
+    end
+    
+    return ans
+end
+
