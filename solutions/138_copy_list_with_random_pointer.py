e75669ee8632d419293cb26748fe16d6ea078f21
Logan Yu <logan.yu@cadre.com>
2020-03-31 18:06:48 -0400

    add O(1) space solution to 138_copy_list_with_random_pointer.py

--- solutions/138_copy_list_with_random_pointer.py
+++ solutions/138_copy_list_with_random_pointer.py
@@ -30,6 +30,7 @@ class Node:
         self.next = next
         self.random = random
 """
+# O(n) space
 class Solution:
     def __init__(self):
         self.visitedHash = {}
@@ -44,3 +45,32 @@ class Solution:
         copy.random = self.copyRandomList(head.random)
         
         return copy 
+
+# O(1) space
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if not head:
+            return head
+        
+        ptr = head
+        while ptr:
+            new_node = Node(ptr.val, None, None)
+            new_node.next = ptr.next
+            ptr.next = new_node
+            ptr = new_node.next
+        
+        ptr = head
+        while ptr:
+            ptr.next.random = ptr.random.next if ptr.random else None
+            ptr = ptr.next.next
+        
+        ptr_old_list = head
+        ptr_new_list = head.next
+        head_new = head.next
+        while ptr_old_list:
+            ptr_old_list.next = ptr_old_list.next.next
+            ptr_new_list.next = ptr_new_list.next.next if ptr_new_list.next else None
+            ptr_old_list = ptr_old_list.next
+            ptr_new_list = ptr_new_list.next
+        
+        return head_new
