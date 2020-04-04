826c72446ca98e6bff13db5d5487128d61ff80db
Logan Yu <logan.yu@cadre.com>
2020-04-03 20:07:40 -0400

    add iterative solutions to 236_lowest_common_ancestor_of_a_binary_tree.py

--- solutions/236_lowest_common_ancestor_of_a_binary_tree.py
+++ solutions/236_lowest_common_ancestor_of_a_binary_tree.py
@@ -33,6 +33,7 @@ p and q are different and both values will exist in the binary tree.
 #         self.left = None
 #         self.right = None
 
+# recursive
 class Solution:
     def __init__(self):
         self.ans = None
@@ -58,3 +59,63 @@ class Solution:
         
         return self.ans
         
+# iterative using parent pointers
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        stack = [root]
+        parent = {root: None}
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+        
+        ancestors = set()
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+        while q not in ancestors:
+            q = parent[q]
+        
+        return q
+
+# iterative without parent pointers
+class Solution:
+    BOTH_PENDING = 2
+    LEFT_DONE = 1
+    BOTH_DONE = 0
+    
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        stack = [(root, Solution.BOTH_PENDING)]
+        one_node_found = False
+        LCA_index = -1
+        
+        while stack:
+            parent_node, parent_state = stack[-1]
+            if parent_state != Solution.BOTH_DONE:
+            
+                if parent_state == Solution.BOTH_PENDING:
+                    if parent_node == p or parent_node == q:
+                        if one_node_found:
+                            return stack[LCA_index][0]
+                        else:
+                            one_node_found = True
+                            LCA_index = len(stack) - 1
+                    child_node = parent_node.left
+                else:
+                    child_node = parent_node.right
+
+                stack.pop()
+                stack.append((parent_node, parent_state - 1))
+
+                if child_node:
+                    stack.append((child_node, Solution.BOTH_PENDING))
+            else:
+                if one_node_found and LCA_index == len(stack) - 1:
+                    LCA_index -= 1
+                stack.pop()
+        
+        return None
