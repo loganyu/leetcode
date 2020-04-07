67bd94677e7eda3d5f14fa9321e44474aaa5b999
Logan Yu <logan.yu@cadre.com>
2020-04-07 14:51:41 -0400

    add 081_search_in_rotated_sorted_array_ii.py

---
+++ solutions/081_search_in_rotated_sorted_array_ii.py
@@ -0,0 +1,43 @@
+'''
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+
+You are given a target value to search. If found in the array return true, otherwise return false.
+
+Example 1:
+
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+Example 2:
+
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+Follow up:
+
+This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+Would this affect the run-time complexity? How and why?
+'''
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = (r - l) // 2 + l
+            while l < m and nums[l] == nums[m]:
+                l += 1
+            if nums[m] == target:
+                return True
+            elif nums[l] <= nums[m]:
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if nums[m] < target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+        return False
+
