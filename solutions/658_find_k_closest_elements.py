c841dde5340406c3a7fb4d9bc7cbaed2e73cbe4a
Logan Yu <logan.yu@cadre.com>
2020-04-15 12:47:59 -0400

    add 658_find_k_closest_elements.py

---
+++ solutions/658_find_k_closest_elements.py
@@ -0,0 +1,27 @@
+'''
+Given a sorted array, two integers k and x, find the k closest elements to x in the array. The result should also be sorted in ascending order. If there is a tie, the smaller elements are always preferred.
+
+Example 1:
+Input: [1,2,3,4,5], k=4, x=3
+Output: [1,2,3,4]
+Example 2:
+Input: [1,2,3,4,5], k=4, x=-1
+Output: [1,2,3,4]
+Note:
+The value k is positive and will always be smaller than the length of the sorted array.
+Length of the given array is positive and will not exceed 104
+Absolute value of elements in the array and x will not exceed 104
+'''
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr) - k
+        while l < r:
+            mid = l + (r - l) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                l = mid + 1
+            else:
+                r = mid
+                
+        return arr[l:l+k]
+
