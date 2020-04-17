2a96d34e09eaa138beebbd9932619ac034554dcd
Logan Yu <logan.yu@cadre.com>
2020-04-17 18:04:01 -0400

    add 680_valid_palindrome_ii.py

---
+++ solutions/680_valid_palindrome_ii.py
@@ -0,0 +1,36 @@
+'''
+Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+Input: "aba"
+Output: True
+Example 2:
+Input: "abca"
+Output: True
+Explanation: You could delete the character 'c'.
+Note:
+The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+'''
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        deleted = False
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return self.is_valid(s, l+1, r) or self.is_valid(s, l, r-1)
+            l += 1
+            r -= 1
+        
+        return True
+    
+    def is_valid(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        
+        return True
+
