9809caefe67dacf1c0adec2695fcdb591f85af45
Logan Yu <logan.yu@cadre.com>
2020-04-18 18:29:15 -0400

    add solution to 350_intersection_of_two_arrays_ii.py

--- solutions/350_intersection_of_two_arrays_ii.py
+++ solutions/350_intersection_of_two_arrays_ii.py
@@ -21,6 +21,8 @@ What if elements of nums2 are stored on disk, and the memory is limited such tha
 '''
 from collections import Counter
 
+# O(n + m)
+# O(1) space
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         counter = Counter(nums1)
@@ -30,5 +32,24 @@ class Solution:
                 sol.append(num)
                 counter[num] -= 1
         
-        return sol
+        return sol  
 
+# O(nlogn + mlogm) time
+# O(1) space
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        ans = []
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                ans.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        
+        return ans
