3b32c382039af83a932cf4ec4e3b26cba0800bac
Logan Yu <logan.yu@cadre.com>
2020-05-08 18:57:08 -0400

    add 188_best_time_to_buy_and_sell_stock_iv.py

---
+++ solutions/188_best_time_to_buy_and_sell_stock_iv.py
@@ -0,0 +1,43 @@
+'''
+Say you have an array for which the i-th element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most k transactions.
+
+Note:
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [2,4,1], k = 2
+Output: 2
+Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+Example 2:
+
+Input: [3,2,6,5,0,3], k = 2
+Output: 7
+Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
+             Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+'''
+
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        if n < 2:
+            return 0
+        if k >= n // 2:
+            profit = 0
+            for i in range(1, len(prices)):
+                if prices[i] > prices[i-1]:
+                    profit += prices[i] - prices[i-1]
+            
+            return profit
+        
+        profits = [[0 for _ in range(n)] for _ in range(k+1)]
+        for i in range(1, k+1):
+            temp_max = -prices[0]
+            for j in range(1, n):
+                profits[i][j] = max(profits[i][j-1], prices[j] + temp_max)
+                temp_max = max(temp_max, profits[i-1][j-1] - prices[j])
+            
+        return profits[k][n-1]
+                
