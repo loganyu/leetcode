8388571aaef7f656dae57a69cada43c82a6fb5bc
Logan Yu <logan.yu@cadre.com>
2020-05-13 14:05:48 -0400

    fix 114_flatten_binay_tree_to_linked_list.py

--- solutions/114_flatten_binay_tree_to_linked_list.py
+++ solutions/114_flatten_binay_tree_to_linked_list.py
@@ -88,3 +88,8 @@ class Solution:
                 rightmost = node.left
                 while rightmost.right:
                     rightmost = rightmost.right
+                rightmost.right = node.right
+                node.right = node.left
+                node.left = None
+            node = node.right 
+            
