37cf9b440fe6aa80872e608778633e176eb85084
Logan Yu <logan.yu@cadre.com>
2020-05-14 13:14:34 -0400

    add 865_smallest_subtree_with_all_the_deepest_nodes.py

---
+++ solutions/865_smallest_subtree_with_all_the_deepest_nodes.py
@@ -0,0 +1,59 @@
+'''
+Given a binary tree rooted at root, the depth of each node is the shortest distance to the root.
+
+A node is deepest if it has the largest depth possible among any node in the entire tree.
+
+The subtree of a node is that node, plus the set of all descendants of that node.
+
+Return the node with the largest depth such that it contains all the deepest nodes in its subtree.
+
+ 
+
+Example 1:
+
+Input: [3,5,1,6,2,0,8,null,null,7,4]
+Output: [2,7,4]
+Explanation:
+
+
+
+We return the node with value 2, colored in yellow in the diagram.
+The nodes colored in blue are the deepest nodes of the tree.
+The input "[3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]" is a serialization of the given tree.
+The output "[2, 7, 4]" is a serialization of the subtree rooted at the node with value 2.
+Both the input and output have TreeNode type.
+ 
+
+Note:
+
+The number of nodes in the tree will be between 1 and 500.
+The values of each node are unique.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        self.deepest = 0
+        self.lca = None
+        
+        self.search(root, 0)
+        
+        return self.lca
+    
+    def search(self, node, depth):
+        self.deepest = max(depth, self.deepest)
+        if not node:
+            return depth
+        left = self.search(node.left, depth+1)
+        right = self.search(node.right, depth+1)
+        
+        if left == right == self.deepest:
+            self.lca = node
+        
+        return max(left, right)
+    
