3d2641149697360f021782a6a6cf20514f1e3fd8
Logan Yu <logan.yu@cadre.com>
2020-06-20 23:09:56 -0400

    add solutions to 1062_longest_repeating_substring.py

--- solutions/1062_longest_repeating_substring.py
+++ solutions/1062_longest_repeating_substring.py
@@ -1,7 +1,7 @@
 '''
 Given a string S, find out the length of the longest repeating substring(s). Return 0 if no repeating substring exists.
 
- 
+
 
 Example 1:
 
@@ -31,20 +31,82 @@ The string S consists of only lowercase English letters from 'a' - 'z'.
 1 <= S.length <= 1500
 '''
 
+# Rabin-Karp O(N logN) time and O(N) space
+
+
+class Solution:
+    def longestRepeatingSubstring(self, S: str) -> int:
+        n = len(S)
+        nums = [ord(S[i]) - ord('a') for i in range(n)]
+        a = 26
+        modulus = 2**32
+
+        l, r = 1, n
+        while l <= r:
+            m = l + (r - l) // 2
+            if self.search(m, a, modulus, n, nums) != -1:
+                l = m + 1
+            else:
+                r = m - 1
+
+        return l - 1
+
+    def search(self, L, a, modulus, n, nums):
+        h = 0
+        for i in range(L):
+            h = (h * a + nums[i]) % modulus
+        seen = set([h])
+        aL = pow(a, L, modulus)
+        for start in range(1, n - L + 1):
+            h = (h * a - nums[start - 1] * aL + nums[start + L - 1]) % modulus
+            if h in seen:
+                return start
+            seen.add(h)
+
+        return -1
+
+
+# O(N^2) time and O(N) space
+class Solution:
+    def longestRepeatingSubstring(self, S: str) -> int:
+        n = len(S)
+        l, r = 1, n
+        while l <= r:
+            m = l + (r - l) // 2
+            if self.search(m, S) != -1:
+                l = m + 1
+            else:
+                r = m - 1
+
+        return l - 1
+
+    def search(self, length, S):
+        n = len(S)
+        seen = set()
+        for s in range(n - length + 1):
+            tmp = S[s:s+length]
+            if tmp in seen:
+                return s
+            seen.add(tmp)
+
+        return -1
+
+# O(N^2) time and O(N) space
+
+
 class Solution:
     def longestRepeatingSubstring(self, S: str) -> int:
         ans = 0
         for i in range(1, len(S)):
-            if ans >= len(S)-i: 
-                break 
-                
+            if ans >= len(S)-i:
+                break
+
             tmp = 0
-            for x, y in zip(S[i:],S[:-i]):
+            for x, y in zip(S[i:], S[:-i]):
                 if x == y:
-                    tmp += 1 
+                    tmp += 1
                     ans = max(ans, tmp)
                 else:
-                    tmp = 0 
-            
-        return ans
+                    tmp = 0
 
+        return ans
