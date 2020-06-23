b399245be1fa7627a145e0749813233b328ef9ca
Logan Yu <logan.yu@cadre.com>
2020-06-23 10:28:48 -0400

    add 222_count_complete_tree_nodes.py

---
+++ solutions/222_count_complete_tree_nodes.py
@@ -0,0 +1,65 @@
+'''
+Given a complete binary tree, count the number of nodes.
+
+Note:
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Example:
+
+Input: 
+    1
+   / \
+  2   3
+ / \  /
+4  5 6
+
+Output: 6
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        d = self.compute_depth(root)
+        if d == 0:
+            return 1
+        
+        left, right = 1, 2**d - 1
+        while left <= right:
+            pivot = left + (right - left) // 2
+            if self.exists(pivot, d, root):
+                left = pivot + 1
+            else:
+                right = pivot - 1
+        
+        return (2**d - 1) + left
+        
+        
+    def compute_depth(self, node):
+        d = 0
+        while node.left:
+            node = node.left
+            d += 1
+        return d
+    
+    def exists(self, idx, d, node):
+        left, right = 0, 2**d - 1
+        for _ in range(d):
+            pivot = left + (right - left) // 2
+            if idx <= pivot:
+                node = node.left
+                right = pivot
+            else:
+                node = node.right
+                left = pivot + 1
+        
+        return node is not None
+    
