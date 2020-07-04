4631ac372d59a8953e5f121f7df19dcb76a8b1c4
Logan Yu <logan.yu@cadre.com>
2020-07-04 08:52:39 -0400

    add 264_ugly_number_ii.py

---
+++ solutions/264_ugly_number_ii.py
@@ -0,0 +1,32 @@
+'''
+Write a program to find the n-th ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. 
+
+Example:
+
+Input: n = 10
+Output: 12
+Explanation: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+Note:  
+
+1 is typically treated as an ugly number.
+n does not exceed 1690.
+'''
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        nums = [1]
+        i2 = i3 = i5 = 0
+        for _ in range(1, n):
+            ugly = min(nums[i2] * 2, nums[i3] * 3, nums[i5] * 5)
+            nums.append(ugly)
+            if ugly == nums[i2] * 2:
+                i2 += 1
+            if ugly == nums[i3] * 3:
+                i3 += 1
+            if ugly == nums[i5] * 5:
+                i5 += 1
+        
+        return nums[n-1]
+        
