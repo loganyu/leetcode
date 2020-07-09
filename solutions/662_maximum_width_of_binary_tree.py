00e0dd79a66519058729be94b41e42c6e09818c3
Logan Yu <logan.yu@cadre.com>
2020-07-09 11:13:59 -0400

    add 662_maximum_width_of_binary_tree.py

---
+++ solutions/662_maximum_width_of_binary_tree.py
@@ -0,0 +1,89 @@
+'''
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+Example 2:
+
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+Example 3:
+
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+Example 4:
+
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+
+
+Note: Answer will in the range of 32-bit signed integer.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+class Solution:
+    def widthOfBinaryTree(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        max_width = 0
+        queue = deque()
+        queue.append((root, 0))
+        
+        while queue:
+            level_head_index = queue[0][1]
+            level_last_index = queue[-1][1]
+            max_width = max(max_width, level_last_index - level_head_index + 1)
+            for _ in range(len(queue)):
+                node, col_index = queue.popleft()
+                if node.left:
+                    queue.append((node.left, 2 * col_index))
+                if node.right:
+                    queue.append((node.right, 2 * col_index + 1))
+        
+        return max_width
+
+
