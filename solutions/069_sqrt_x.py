5d371df3d8229a4b0460bd4dac35543e3a3c49d5
Logan Yu <logan.yu@cadre.com>
2020-08-12 12:25:53 -0400

    add 069_sqrt_x.py

--- solutions/069_sqrt_x.py
+++ solutions/069_sqrt_x.py
@@ -29,4 +29,16 @@ class Solution:
                 r = m - 1
             else:
                 l = m + 1
-               
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 0, x+1
+        while left < right:
+            mid = left + (right - left) // 2
+            if mid**2 > x:
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left - 1
+                       
