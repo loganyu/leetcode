3c38d0c48f48282c609fd9225106cf62c428291b
Logan Yu <logan.yu@cadre.com>
2020-08-12 12:48:34 -0400

    add sol to 035_search_insert_position.py

--- solutions/035_search_insert_position.py
+++ solutions/035_search_insert_position.py
@@ -35,3 +35,15 @@ class Solution:
                 r = m - 1
         return l
     
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)
+        while l < r:
+            m = l + (r - l) // 2
+            if nums[m] >= target:
+                r = m
+            else:
+                l = m + 1
+        
+        return l
