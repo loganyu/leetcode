1545d6b12152e3fd716e23c1231c03b69313c98c
Logan Yu <logan.yu@cadre.com>
2020-10-12 10:43:22 -0400

    add 859_buddy_strings.py

---
+++ solutions/859_buddy_strings.py
@@ -0,0 +1,60 @@
+'''
+Given two strings A and B of lowercase letters, return true if you can swap two letters in A so the result is equal to B, otherwise, return false.
+
+Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at A[i] and A[j]. For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
+
+ 
+
+Example 1:
+
+Input: A = "ab", B = "ba"
+Output: true
+Explanation: You can swap A[0] = 'a' and A[1] = 'b' to get "ba", which is equal to B.
+Example 2:
+
+Input: A = "ab", B = "ab"
+Output: false
+Explanation: The only letters you can swap are A[0] = 'a' and A[1] = 'b', which results in "ba" != B.
+Example 3:
+
+Input: A = "aa", B = "aa"
+Output: true
+Explanation: You can swap A[0] = 'a' and A[1] = 'a' to get "aa", which is equal to B.
+Example 4:
+
+Input: A = "aaaaaaabc", B = "aaaaaaacb"
+Output: true
+Example 5:
+
+Input: A = "", B = "aa"
+Output: false
+ 
+
+Constraints:
+
+0 <= A.length <= 20000
+0 <= B.length <= 20000
+A and B consist of lowercase letters.
+
+'''
+
+class Solution:
+    def buddyStrings(self, A: str, B: str) -> bool:
+        if len(A) != len(B):
+            return False
+        if A == B:
+            seen = set()
+            for a in A:
+                if a in seen:
+                    return True
+                seen.add(a)
+            return False
+        else:
+            pairs = []
+            for a, b in zip(A, B):
+                if a != b:
+                    pairs.append((a,b))
+                if len(pairs) >= 3:
+                    return False
+            return len(pairs) == 2 and pairs[0] == pairs[1][::-1]
+        
