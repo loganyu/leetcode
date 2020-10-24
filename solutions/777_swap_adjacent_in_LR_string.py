3806c3ae26c291d0210d37b384825176249f329e
Logan Yu <logan.yu@cadre.com>
2020-10-24 12:51:39 -0400

    add 777_swap_adjacent_in_LR_string.py

---
+++ solutions/777_swap_adjacent_in_LR_string.py
@@ -0,0 +1,68 @@
+'''
+In a string composed of 'L', 'R', and 'X' characters, like "RXXLRXRXL", a move consists of either replacing one occurrence of "XL" with "LX", or replacing one occurrence of "RX" with "XR". Given the starting string start and the ending string end, return True if and only if there exists a sequence of moves to transform one string to the other.
+
+ 
+
+Example 1:
+
+Input: start = "RXXLRXRXL", end = "XRLXXRRLX"
+Output: true
+Explanation: We can transform start to end following these steps:
+RXXLRXRXL ->
+XRXLRXRXL ->
+XRLXRXRXL ->
+XRLXXRRXL ->
+XRLXXRRLX
+Example 2:
+
+Input: start = "X", end = "L"
+Output: false
+Example 3:
+
+Input: start = "LLR", end = "RRL"
+Output: false
+Example 4:
+
+Input: start = "XL", end = "LX"
+Output: true
+Example 5:
+
+Input: start = "XLLR", end = "LXLX"
+Output: false
+ 
+
+Constraints:
+
+1 <= start.length <= 104
+start.length == end.length
+Both start and end will only consist of characters in 'L', 'R', and 'X'.
+'''
+
+class Solution:
+    def canTransform(self, start: str, end: str) -> bool:
+        if start.count('X') != end.count('X'):
+            return False
+        
+        n = len(start)
+        i = j = 0
+        while (i < n and j < n):
+            while i < n and start[i] == 'X':
+                i += 1
+            while j < n and end[j] == 'X':
+                j += 1
+            
+            if i == n or j == n:
+                return i == n and j == n
+            
+            if start[i] != end[j]:
+                return False
+            if start[i] == 'L' and i < j:
+                return False
+            if start[i] == 'R' and i > j:
+                return False
+            
+            i += 1
+            j += 1
+        
+        return True
+
