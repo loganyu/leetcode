bd4e7c658a487aef68e24f092391d5196b82909a
Logan Yu <logan.yu@cadre.com>
2020-11-09 07:24:33 -0500

    add 1026_maximum_difference_between_node_and_ancestor.py

---
+++ solutions/1026_maximum_difference_between_node_and_ancestor.py
@@ -0,0 +1,54 @@
+'''
+Given the root of a binary tree, find the maximum value V for which there exist different nodes A and B where V = |A.val - B.val| and A is an ancestor of B.
+
+A node A is an ancestor of B if either: any child of A is equal to B, or any child of A is an ancestor of B.
+
+ 
+
+Example 1:
+
+
+Input: root = [8,3,10,1,6,null,14,null,null,4,7,13]
+Output: 7
+Explanation: We have various ancestor-node differences, some of which are given below :
+|8 - 3| = 5
+|3 - 7| = 4
+|8 - 1| = 7
+|10 - 13| = 3
+Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
+Example 2:
+
+
+Input: root = [1,null,2,null,0,3]
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 5000].
+0 <= Node.val <= 105
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        def helper(node, cur_max, cur_min):
+            if not node:
+                return cur_max - cur_min
+            cur_max = max(cur_max, node.val)
+            cur_min = min(cur_min, node.val)
+            left = helper(node.left, cur_max, cur_min)
+            right = helper(node.right, cur_max, cur_min)
+            
+            return max(left, right)
+        
+        return helper(root, root.val, root.val)
+        
