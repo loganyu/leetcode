c83c4df81d1dd70b29438c3f82b12bd20d5fc270
Logan Yu <logan.yu@cadre.com>
2020-11-26 13:36:59 -0500

    add 977_squares_of_a_sorted_array.java

---
+++ solutions/977_squares_of_a_sorted_array.java
@@ -0,0 +1,57 @@
+/*
+Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Example 2:
+
+Input: [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+
+Note:
+
+1 <= A.length <= 10000
+-10000 <= A[i] <= 10000
+A is sorted in non-decreasing order.
+*/
+
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        int N = A.length;
+        int j = 0;
+        while (j < N && A[j] < 0) {
+            j++;
+        }
+        int i = j-1;
+        
+        int[] ans = new int[N];
+        int t = 0;
+        
+        while (i >= 0 && j < N) {
+            if (A[i] * A[i] < A[j] * A[j]) {
+                ans[t++] = A[i] * A[i];
+                i--;
+            } else {
+                ans[t++] = A[j] * A[j];
+                j++;
+            }
+        }
+        
+        while (i >= 0) {
+            ans[t++] = A[i] * A[i];
+            i--;
+        }
+        while (j < N) {
+            ans[t++] = A[j] * A[j];
+            j++;
+        }
+        
+        return ans;
+    }
+}
+
