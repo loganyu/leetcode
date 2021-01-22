a809df7deb5090bcf7a5733e81cf9759f5572acf
Logan Yu <yu.logan@gmail.com>
2021-01-22 09:42:03 -0500

    move files

--- solutions/1574_shortest_subarray_to_be_removed_to_make_array_sorted.py
+++ solutions/1574_shortest_subarray_to_be_removed_to_make_array_sorted.py
@@ -37,22 +37,21 @@ Constraints:
 
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
-        l, r = 0, len(arr) - 1
-        while l < r and arr[l] <= arr[l+1]:
-            l += 1
-        if l == len(arr) - 1:
-            return 0
-        while r > 0 and arr[r-1] <= arr[r]:
-            r -= 1
-        to_remove = min(len(arr) - l - 1, r)
-        
-        for iL in range(l+1):
-            if arr[iL] <= arr[r]:
-                to_remove = min(to_remove, r - iL - 1)
-            elif r < len(arr) - 1:
-                r += 1
-            else:
+        n = len(arr)
+        r = n - 1
+        while r > 0:
+            if arr[r-1] > arr[r]:
                 break
+            r -= 1
+        if r == 0:
+            return 0
+        res = r
+        for l in range(r):
+            if l > 0 and arr[l-1] > arr[l]:
+                break
+            while r < n and arr[l] > arr[r]:
+                r += 1
+            res = min(res, r - l - 1)
         
-        return to_remove
+        return res
         
