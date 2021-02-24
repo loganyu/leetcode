656e60f6549568fecae305a01e87b757d70c5643
Logan Yu <yu.logan@gmail.com>
2021-02-24 13:02:27 -0500

    add 856_score_of_parentheses.java

---
+++ solutions/856_score_of_parentheses.java
@@ -0,0 +1,43 @@
+/*
+Given a balanced parentheses string S, compute the score of the string based on the following rule:
+
+() has score 1
+AB has score A + B, where A and B are balanced parentheses strings.
+(A) has score 2 * A, where A is a balanced parentheses string.
+ 
+
+Example 1:
+
+Input: "()"
+Output: 1
+Example 2:
+
+Input: "(())"
+Output: 2
+Example 3:
+
+Input: "()()"
+Output: 2
+Example 4:
+
+Input: "(()(()))"
+Output: 6
+*/
+
+class Solution {
+    public int scoreOfParentheses(String S) {
+        int ans = 0, bal = 0;
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '(') {
+                bal++;
+            } else {
+                bal--;
+                if (S.charAt(i-1) == '(')
+                    ans += 1 << bal;
+            }
+        }
+        
+        return ans;
+    }
+}
+
