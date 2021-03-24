700d2f23b1f7a354483c472d52dfdaa74f5d6153
Logan Yu <yu.logan@gmail.com>
2021-03-24 11:05:50 -0400

    add 870_advantage_shuffle.py

---
+++ solutions/870_advantage_shuffle.py
@@ -0,0 +1,43 @@
+'''
+Given two arrays A and B of equal size, the advantage of A with respect to B is the number of indices i for which A[i] > B[i].
+
+Return any permutation of A that maximizes its advantage with respect to B.
+
+ 
+
+Example 1:
+
+Input: A = [2,7,11,15], B = [1,10,4,11]
+Output: [2,11,7,15]
+Example 2:
+
+Input: A = [12,24,8,32], B = [13,25,32,11]
+Output: [24,32,8,12]
+ 
+
+Note:
+
+1 <= A.length = B.length <= 10000
+0 <= A[i] <= 10^9
+0 <= B[i] <= 10^9
+
+'''
+
+class Solution:
+    def advantageCount(self, A: List[int], B: List[int]) -> List[int]:
+        sortedA = sorted(A)
+        sortedB = sorted(B)
+        assigned = {b: [] for b in B}
+        remaining = []
+        
+        j = 0
+        for a in sortedA:
+            if a > sortedB[j]:
+                assigned[sortedB[j]].append(a)
+                j += 1
+            else:
+                remaining.append(a)
+        
+        return [assigned[b].pop() if assigned[b] else remaining.pop() for b in B]
+        
+
