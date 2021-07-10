c7b15bb7ee40c6184b4e61d0e40ee90ec699236d
Logan Yu <yu.logan@gmail.com>
2021-07-10 13:03:46 -0400

    add 1925_count_square_sum_triples.py

---
+++ 1925_count_square_sum_triples.py
@@ -0,0 +1,36 @@
+'''
+A square triple (a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2.
+
+Given an integer n, return the number of square triples such that 1 <= a, b, c <= n.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: 2
+Explanation: The square triples are (3,4,5) and (4,3,5).
+Example 2:
+
+Input: n = 10
+Output: 4
+Explanation: The square triples are (3,4,5), (4,3,5), (6,8,10), and (8,6,10).
+ 
+
+Constraints:
+
+1 <= n <= 250
+'''
+
+class Solution:
+    def countTriples(self, n: int) -> int:
+        count = 0
+        for a in range(1, n+1):
+            for b in range(1, n+1):
+                total = a**2 + b**2
+                c = int(math.sqrt(total))
+                if (c**2 == total and c <= n):
+                    count += 1
+                
+        return count 
+
