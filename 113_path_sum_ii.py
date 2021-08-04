df5f2a73dcc3990253de60a545dd817727251186
Logan Yu <yu.logan@gmail.com>
2021-08-04 10:40:59 -0400

    add 113_path_sum_ii.py

---
+++ 113_path_sum_ii.py
@@ -0,0 +1,53 @@
+'''
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where each path's sum equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        if not root:
+            return []
+        sol = []
+        stack = [(root, [root.val])]
+        while stack:
+            curr, ls = stack.pop()
+            if not curr.left and not curr.right and sum(ls) == targetSum:
+                sol.append(ls)
+            if curr.right:
+                stack.append((curr.right, ls + [curr.right.val]))
+            if curr.left:
+                stack.append((curr.left, ls + [curr.left.val]))
+        
+        return sol
+
