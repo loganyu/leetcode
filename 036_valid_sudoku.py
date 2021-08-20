40061a3bb03d677af62442f4c228dbab06818139
Logan Yu <yu.logan@gmail.com>
2021-08-20 10:08:43 -0400

    add 036_valid_sudoku.py

---
+++ 036_valid_sudoku.py
@@ -0,0 +1,78 @@
+'''
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+Example 1:
+
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ 
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit or '.'.
+
+'''
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [0] * 9
+        cols = [0] * 9
+        boxes = [0] * 9
+        
+        for r in range(9):
+            for c in range(9):
+                pos = board[r][c]
+                if pos == '.':
+                    continue
+                pos = int(pos) - 1
+                
+                if rows[r] & (1 << pos):
+                    return False
+                rows[r] |= (1 << pos)
+                
+                if cols[c] & (1 << pos):
+                    return False
+                cols[c] |= (1 << pos)
+                
+                idx = (r // 3) * 3 + c // 3
+                if boxes[idx] & (1 << pos):
+                    return False
+                boxes[idx] |= (1 << pos)
+        
+        return True
+        
