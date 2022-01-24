2e0c3e9b1a63b9136a07e892bed1e5bb18de14eb
Logan Yu <yu.logan@gmail.com>
2022-01-24 00:00:11 -0500

    add 1291_sequential_digits.java

---
+++ 1291_sequential_digits.java
@@ -0,0 +1,40 @@
+/*
+An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+
+Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+
+ 
+
+Example 1:
+
+Input: low = 100, high = 300
+Output: [123,234]
+Example 2:
+
+Input: low = 1000, high = 13000
+Output: [1234,2345,3456,4567,5678,6789,12345]
+ 
+
+Constraints:
+
+10 <= low <= high <= 10^9
+*/
+
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> res = new ArrayList<>();
+        for (int digit = 1; digit < 9; digit++) {
+            int next = digit, n = next;
+            while (n <= high && next < 10) {
+                if (n >= low) {
+                    res.add(n);
+                }
+                n = n * 10 + ++next;
+            }
+        }
+        Collections.sort(res);
+        
+        return res;
+    }
+}
+
