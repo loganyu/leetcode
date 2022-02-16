c731525dd12209a671613773e6deeea38ad5123d
Logan Yu <yu.logan@gmail.com>
2022-02-15 21:44:55 -0500

    add 104_maximum_depth_of_binary_tree.rs

---
+++ 104_maximum_depth_of_binary_tree.rs
@@ -0,0 +1,59 @@
+/*
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100
+*/
+
+
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(node) => {
+                let left_height = Self::max_depth(node.borrow().left.clone());
+                let right_height = Self::max_depth(node.borrow().right.clone());
+                return std::cmp::max(left_height, right_height) + 1;
+            }
+            None => return 0,
+        }
+        
+    }
+}
+
