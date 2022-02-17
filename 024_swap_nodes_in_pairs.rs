abbdfee5c1d8cb49a9a60198c8d460f58996cf04
Logan Yu <yu.logan@gmail.com>
2022-02-16 20:41:21 -0500

    add 024_swap_nodes_in_pairs.rs

---
+++ 024_swap_nodes_in_pairs.rs
@@ -0,0 +1,56 @@
+/*
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+Example 2:
+
+Input: head = []
+Output: []
+Example 3:
+
+Input: head = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 100].
+0 <= Node.val <= 100
+*/
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn swap_pairs(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        head.and_then(|mut n| {
+            match n.next {
+                Some(mut m) => {
+                    n.next = Solution::swap_pairs(m.next);
+                    m.next = Some(n);
+                    Some(m)
+                },
+                None => Some(n)
+            }
+        })
+    }
+}
