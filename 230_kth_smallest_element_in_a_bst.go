6eec5e071e46bfb7b3b245599a418395e6d663ae
Logan Yu <yu.logan@gmail.com>
2022-04-23 20:53:28 -0400

    add 230_kth_smallest_element_in_a_bst.go

---
+++ 230_kth_smallest_element_in_a_bst.go
@@ -0,0 +1,50 @@
+/*
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+ 
+
+Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthSmallest(root *TreeNode, k int) int {
+    stack := []*TreeNode{}
+    for {
+        for root != nil {
+            stack = append(stack, root)
+            root = root.Left
+        }
+        root = stack[len(stack) - 1]
+        stack = stack[:len(stack) - 1]
+        k--
+        if k == 0 { return root.Val }
+        root = root.Right
+    }
+}
+
