f928e07653120a99162c7148f021699a16b79fc5
Logan Yu <yu.logan@gmail.com>
2022-05-22 21:22:14 -0400

    add 647_palindromic_substrings.java

---
+++ 647_palindromic_substrings.java
@@ -0,0 +1,46 @@
+/*
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+*/
+
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int center = 0; center < 2*n - 1; center++) {
+            int left = center / 2;
+            int right = left + center % 2;
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                ans++;
+                left--;
+                right++;
+            }
+            
+        }
+        
+        return ans;
+    }
+}
+
