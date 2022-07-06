09bce479d46712c869b51853cd11e917c75fb437
Logan Yu <yu.logan@gmail.com>
2022-07-05 20:41:23 -0400

    add 128_longest_consecutive_sequence.go

---
+++ 128_longest_consecutive_sequence.go
@@ -0,0 +1,45 @@
+/*
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/
+
+func longestConsecutive(nums []int) int {
+    unique := map[int]bool{}
+    for _, num := range nums {
+        unique[num] = true
+    }
+    ans := 0
+    for num := range unique {
+        if unique[num-1] {
+            continue
+        }
+        count := 0
+        for i := num; unique[i]; i++ {
+            count++
+        }
+        if ans < count {
+            ans = count
+        }
+    }
+    return ans
+}
+
