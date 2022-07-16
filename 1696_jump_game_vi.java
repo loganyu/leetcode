d067cba4bfec9cf4b733b4ffe9f567fd2594a91e
Logan Yu <yu.logan@gmail.com>
2022-07-15 21:06:22 -0400

    add 1696_jump_game_vi.java

---
+++ 1696_jump_game_vi.java
@@ -0,0 +1,49 @@
+/*
+You are given a 0-indexed integer array nums and an integer k.
+
+You are initially standing at index 0. In one move, you can jump at most k steps forward without going outside the boundaries of the array. That is, you can jump from index i to any index in the range [i + 1, min(n - 1, i + k)] inclusive.
+
+You want to reach the last index of the array (index n - 1). Your score is the sum of all nums[j] for each index j you visited in the array.
+
+Return the maximum score you can get.
+
+ 
+
+Example 1:
+
+Input: nums = [1,-1,-2,4,-7,3], k = 2
+Output: 7
+Explanation: You can choose your jumps forming the subsequence [1,-1,4,3] (underlined above). The sum is 7.
+Example 2:
+
+Input: nums = [10,-5,-2,4,0,3], k = 3
+Output: 17
+Explanation: You can choose your jumps forming the subsequence [10,4,3] (underlined above). The sum is 17.
+Example 3:
+
+Input: nums = [1,-5,-20,4,-1,3,-6,-3], k = 2
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length, k <= 105
+-104 <= nums[i] <= 104
+*/
+
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.offer(0);
+        for (int i = 1; i < n; ++i) {
+            nums[i] = nums[dq.peekFirst()] + nums[i];
+            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) dq.pollLast();
+            dq.offerLast(i);
+            if (i - dq.peekFirst() >= k) dq.pollFirst();
+        }
+        
+        return nums[n - 1];
+    }
+}
+
