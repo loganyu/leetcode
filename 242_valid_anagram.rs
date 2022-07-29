b3e50c80273e4da5ef7874257e5cf9c3ea82335d
Logan Yu <yu.logan@gmail.com>
2022-07-28 20:11:51 -0400

    add 242_valid_anagram.rs

---
+++ 242_valid_anagram.rs
@@ -0,0 +1,44 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+*/
+
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        let mut s_map = HashMap::new();
+        let mut t_map = HashMap::new();
+
+        for letter in s.chars() {
+            *s_map.entry(letter).or_insert(0) += 1;
+        }
+        for letter in t.chars() {
+            *t_map.entry(letter).or_insert(0) += 1;
+        }
+        return s_map == t_map;
+    }
+}
+
