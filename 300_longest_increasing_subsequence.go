e4615c6c5d5ded0cb5165e22b3e8f4b9f2a61a2d
Logan Yu <yu.logan@gmail.com>
2022-08-11 22:18:40 -0400

    add 300_longest_increasing_subsequence.go

---
+++ 300_longest_increasing_subsequence.go
@@ -0,0 +1,66 @@
+/*
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+ 
+
+Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+*/
+
+func lengthOfLIS(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+    
+    dp := make([]int, len(nums))
+    dp[0] = nums[0]
+    
+    hi := 0
+    
+    for i := 1; i < len(nums); i++ {
+        idx := biSearchIdx(dp, nums[i], 0, hi)
+        dp[idx] = nums[i]
+        if idx == hi+1 {
+            hi++
+        }
+    }
+    
+    return hi+1
+}
+
+func biSearchIdx(dp []int, target, lo, hi int) int {
+    for lo <= hi {
+        mid := lo+(hi-lo)/2
+        if dp[mid] > target {
+            hi = mid-1
+        } else if dp[mid] < target {
+            lo = mid+1
+        } else {
+            return mid
+        }
+    }
+    return lo
+}
+
