d383dd221a594c44886529393617c222014c5616
Logan Yu <yu.logan@gmail.com>
2022-10-28 13:28:49 -0400

    update 835_image_overlap.py

--- 835_image_overlap.py
+++ 835_image_overlap.py
@@ -27,18 +27,21 @@ class Solution:
     def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
         dim = len(A)
         def shift_and_count(x_shift, y_shift, M, R):
-            count = 0
+            left_shift_count, right_shift_count = 0, 0
             for r_row, m_row in enumerate(range(y_shift, dim)):
                 for r_col, m_col in enumerate(range(x_shift, dim)):
                     if M[m_row][m_col] == 1 and M[m_row][m_col] == R[r_row][r_col]:
-                        count += 1
-            return count
-        
+                        left_shift_count += 1
+                    if M[m_row][r_col] == 1 and M[m_row][r_col] == R[r_row][m_col]:
+                        right_shift_count += 1
+
+            return max(left_shift_count, right_shift_count)
+
         max_overlaps = 0
         for y_shift in range(0, dim):
             for x_shift in range(0, dim):
                 max_overlaps = max(max_overlaps, shift_and_count(x_shift, y_shift, A, B))
                 max_overlaps = max(max_overlaps, shift_and_count(x_shift, y_shift, B, A))
-        
+
         return max_overlaps
-        
+
