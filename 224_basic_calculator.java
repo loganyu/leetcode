6fd2da7fc1a12d8092b1e3e6ad64b91ca6afe74a
Logan Yu <yu.logan@gmail.com>
2022-11-20 17:24:41 -0500

    add 224_basic_calculator.java

---
+++ 224_basic_calculator.java
@@ -0,0 +1,62 @@
+/*
+Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
+
+Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+
+ 
+
+Example 1:
+
+Input: s = "1 + 1"
+Output: 2
+Example 2:
+
+Input: s = " 2-1 + 2 "
+Output: 3
+Example 3:
+
+Input: s = "(1+(4+5+2)-3)+(6+8)"
+Output: 23
+ 
+
+Constraints:
+
+1 <= s.length <= 3 * 105
+s consists of digits, '+', '-', '(', ')', and ' '.
+s represents a valid expression.
+'+' is not used as a unary operation (i.e., "+1" and "+(2 + 3)" is invalid).
+'-' could be used as a unary operation (i.e., "-1" and "-(2 + 3)" is valid).
+There will be no two consecutive operators in the input.
+Every number and running calculation will fit in a signed 32-bit integer.
+*/
+
+class Solution {
+    public int calculate(String s) {
+        int length = s.length(), sign = 1, result = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < length; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                int sum = s.charAt(i) - '0';
+                while (i + 1 < length && Character.isDigit(s.charAt(i + 1))) {
+                    sum = sum * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                result += sum * sign;
+            } else if (s.charAt(i) == '+')
+                sign = 1;
+            else if (s.charAt(i) == '-')
+                sign = -1;
+            else if (s.charAt(i) == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (s.charAt(i) == ')') {
+                result = result * stack.pop() + stack.pop();
+            }
+
+        }
+        return result;
+    }
+}
+
