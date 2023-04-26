dc40be4fc6e2f80238863190c5181eaa6a724ddd
Logan Yu <yu.logan@gmail.com>
2023-04-25 22:05:57 -0500

    add 2336_smallest_number_in_infinite_set.py

---
+++ problems/2336_smallest_number_in_infinite_set.py
@@ -0,0 +1,70 @@
+'''
+You have a set which contains all positive integers [1, 2, 3, 4, 5, ...].
+
+Implement the SmallestInfiniteSet class:
+
+SmallestInfiniteSet() Initializes the SmallestInfiniteSet object to contain all positive integers.
+int popSmallest() Removes and returns the smallest integer contained in the infinite set.
+void addBack(int num) Adds a positive integer num back into the infinite set, if it is not already in the infinite set.
+ 
+
+Example 1:
+
+Input
+["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest", "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+[[], [2], [], [], [], [1], [], [], []]
+Output
+[null, null, 1, 2, 3, null, 1, 4, 5]
+
+Explanation
+SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
+smallestInfiniteSet.addBack(2);    // 2 is already in the set, so no change is made.
+smallestInfiniteSet.popSmallest(); // return 1, since 1 is the smallest number, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 2, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 3, and remove it from the set.
+smallestInfiniteSet.addBack(1);    // 1 is added back to the set.
+smallestInfiniteSet.popSmallest(); // return 1, since 1 was added back to the set and
+                                   // is the smallest number, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 4, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 5, and remove it from the set.
+ 
+
+Constraints:
+
+1 <= num <= 1000
+At most 1000 calls will be made in total to popSmallest and addBack.
+'''
+
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.is_present: {int} = set()
+        self.added_integers: [int] = []
+        self.current_integer = 1
+
+    def popSmallest(self) -> int:
+        # If there are numbers in the min-heap, 
+        # top element is lowest among all the available numbers.
+        if len(self.added_integers):
+            answer = heapq.heappop(self.added_integers)
+            self.is_present.remove(answer)
+        # Otherwise, the smallest number of large positive set 
+        # denoted by 'current_integer' is the answer.
+        else:
+            answer = self.current_integer
+            self.current_integer += 1
+        return answer
+
+    def addBack(self, num: int) -> None:
+        if self.current_integer <= num or num in self.is_present:
+            return
+        # We push 'num' in the min-heap if it isn't already present.
+        heapq.heappush(self.added_integers, num)
+        self.is_present.add(num)
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
+
