38267c355476f96faad9f0d078b14eb2e9af7f01
Logan Yu <yu.logan@gmail.com>
2023-07-10 10:43:40 -0700

    add 111_minimum_depth_of_binary_tree.rs

---
+++ problems/111_minimum_depth_of_binary_tree.rs
@@ -0,0 +1,64 @@
+/*
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 2
+Example 2:
+
+Input: root = [2,null,3,null,4,null,5,null,6]
+Output: 5
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 105].
+-1000 <= Node.val <= 1000
+*/
+
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn min_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(node) => {
+                let left = Self::min_depth(node.borrow().left.clone());
+                let right = Self::min_depth(node.borrow().right.clone());
+
+                if left == 0 || right == 0 {
+                    return left.max(right) + 1
+                }
+                
+                left.min(right) + 1
+            },
+            None => 0,
+        }
+    }
+}
+
