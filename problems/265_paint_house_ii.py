d8d5d13b46e8d6feb12a86d48a8b562b8681f1c3
Logan Yu <yu.logan@gmail.com>
2023-07-18 05:17:08 +0000

    add 265_paint_house_ii.py

---
+++ problems/265_paint_house_ii.py
@@ -0,0 +1,52 @@
+'''
+There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+
+The cost of painting each house with a certain color is represented by an n x k cost matrix costs.
+
+For example, costs[0][0] is the cost of painting house 0 with color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on...
+Return the minimum cost to paint all houses.
+
+
+
+Example 1:
+
+Input: costs = [[1,5,3],[2,9,4]]
+Output: 5
+Explanation:
+Paint house 0 into color 0, paint house 1 into color 2. Minimum cost: 1 + 4 = 5;
+Or paint house 0 into color 2, paint house 1 into color 0. Minimum cost: 3 + 2 = 5.
+Example 2:
+
+Input: costs = [[1,3],[2,4]]
+Output: 5
+
+
+Constraints:
+
+costs.length == n
+costs[i].length == k
+1 <= n <= 100
+2 <= k <= 20
+1 <= costs[i][j] <= 20
+
+
+Follow up: Could you solve it in O(nk) runtime?
+'''
+
+class Solution:
+    def minCostII(self, costs: List[List[int]]) -> int:
+        n = len(costs)
+        if n == 0:
+            return 0
+        k = len(costs[0])
+
+        for house in range(1, n):
+            for color in range(k):
+                best = math.inf
+                for previous_color in range(k):
+                    if color == previous_color: continue
+                    best = min(best, costs[house - 1][previous_color])
+                costs[house][color] += best
+
+        return min(costs[-1])
+
