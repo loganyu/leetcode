a4f2d38f0fd5de003674085606fb60e5b3f9b818
Logan Yu <yu.logan@gmail.com>
2023-11-22 00:55:43 +0000

    add 1424_diagonal_traverse_ii.py

---
+++ problems/1424_diagonal_traverse_ii.py
@@ -0,0 +1,42 @@
+'''
+Given a 2D integer array nums, return all elements of nums in diagonal order as shown in the below images.
+
+
+
+Example 1:
+
+
+Input: nums = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,4,2,7,5,3,8,6,9]
+Example 2:
+
+
+Input: nums = [[1,2,3,4,5],[6,7],[8],[9,10,11],[12,13,14,15,16]]
+Output: [1,6,2,8,7,3,9,4,12,10,5,13,11,14,15,16]
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i].length <= 105
+1 <= sum(nums[i].length) <= 105
+1 <= nums[i][j] <= 105
+'''
+
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        queue = deque([(0, 0)])
+        ans = []
+
+        while queue:
+            row, col = queue.popleft()
+            ans.append(nums[row][col])
+
+            if col == 0 and row + 1 < len(nums):
+                queue.append((row + 1, col))
+
+            if col + 1 < len(nums[row]):
+                queue.append((row, col + 1))
+
+        return ans
+
