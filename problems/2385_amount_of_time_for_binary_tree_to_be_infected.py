33d1d0f82f7247b1617da07a1d6d1a46812da06c
Logan Yu <yu.logan@gmail.com>
2024-01-12 02:29:33 +0000

    add 2385_amount_of_time_for_binary_tree_to_be_infected.py

---
+++ problems/2385_amount_of_time_for_binary_tree_to_be_infected.py
@@ -0,0 +1,73 @@
+'''
+You are given the root of a binary tree with unique values, and an integer start. At minute 0, an infection starts from the node with value start.
+
+Each minute, a node becomes infected if:
+
+The node is currently uninfected.
+The node is adjacent to an infected node.
+Return the number of minutes needed for the entire tree to be infected.
+
+
+
+Example 1:
+
+
+Input: root = [1,5,3,null,4,10,6,9,2], start = 3
+Output: 4
+Explanation: The following nodes are infected during:
+- Minute 0: Node 3
+- Minute 1: Nodes 1, 10 and 6
+- Minute 2: Node 5
+- Minute 3: Node 4
+- Minute 4: Nodes 9 and 2
+It takes 4 minutes for the whole tree to be infected so we return 4.
+Example 2:
+
+
+Input: root = [1], start = 1
+Output: 0
+Explanation: At minute 0, the only node in the tree is infected so we return 0.
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 105].
+1 <= Node.val <= 105
+Each node has a unique value.
+A node with a value of start exists in the tree.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.max_distance = 0
+
+    def amountOfTime(self, root, start):
+        self.traverse(root, start)
+        return self.max_distance
+
+    def traverse(self, root, start):
+        depth = 0
+        if root is None:
+            return depth
+
+        left_depth = self.traverse(root.left, start)
+        right_depth = self.traverse(root.right, start)
+
+        if root.val == start:
+            self.max_distance = max(left_depth, right_depth)
+            depth = -1
+        elif left_depth >= 0 and right_depth >= 0:
+            depth = max(left_depth, right_depth) + 1
+        else:
+            distance = abs(left_depth) + abs(right_depth)
+            self.max_distance = max(self.max_distance, distance)
+            depth = min(left_depth, right_depth) - 1
+
+        return depth
+
