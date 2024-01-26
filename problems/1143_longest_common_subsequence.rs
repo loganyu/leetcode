3b5311618aa63dfa8deefcd6701c63483bdb009b
Logan Yu <yu.logan@gmail.com>
2024-01-26 03:19:10 +0000

    add 1143_longest_common_subsequence.rs

---
+++ problems/1143_longest_common_subsequence.rs
@@ -0,0 +1,55 @@
+/*
+ * Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace"
+Output: 3
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+
+
+Constraints:
+
+1 <= text1.length, text2.length <= 1000
+text1 and text2 consist of only lowercase English characters.
+*/
+
+impl Solution {
+  pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
+    let s1 = text1.chars().collect::<Vec<char>>();
+    let s2 = text2.chars().collect::<Vec<char>>();
+
+    let n = s1.len();
+    let m = s2.len();
+    let mut memo = vec![vec![0;m+1];n+1];
+
+    for i in 0..n {
+      for j in 0..m {
+        if s1[i] == s2[j] {
+          memo[i+1][j+1] = std::cmp::max(memo[i+1][j+1], memo[i][j]+1);
+        } else {
+          memo[i+1][j+1] = memo[i+1][j+1].max(memo[i+1][j]).max(memo[i][j+1]);
+        }
+      }
+    }
+    memo[n][m]
+  }
+}
+
