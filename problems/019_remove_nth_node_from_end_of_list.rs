2e2b596c91ea340379a104e15e761444c709db39
Logan Yu <yu.logan@gmail.com>
2024-03-04 00:39:56 +0000

    add 019_remove_nth_node_from_end_of_list.rs

---
+++ problems/019_remove_nth_node_from_end_of_list.rs
@@ -0,0 +1,67 @@
+/*
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+
+
+Follow up: Could you do this in one pass?
+*/
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        let mut dummy = Box::new(ListNode { val: 0, next: head });
+        let mut fast = dummy.clone();
+        for _ in 0..n as usize {
+            fast = fast.next.unwrap();
+        }
+
+        let mut slow = dummy.as_mut();
+        while fast.next.is_some() {
+            fast = fast.next.unwrap();
+            slow = slow.next.as_mut().unwrap();
+        }
+
+        let next = slow.next.as_ref().unwrap();
+        slow.next = next.next.clone();
+        dummy.next
+    }
+}
+
