e5cfec4cb1a7e8bfb8cbad91f7ddf44077ef35ae
Logan Yu <yu.logan@gmail.com>
2024-03-09 04:36:19 +0000

    add 876_middle_of_the_linked_list.rs

---
+++ problems/876_middle_of_the_linked_list.rs
@@ -0,0 +1,56 @@
+/*
+ * Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+*/
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let (mut slow, mut fast) = (head.clone(), head.and_then(|n| n.next));
+
+        while let Some(n) = fast {
+            slow = slow.and_then(|s| s.next);
+            fast = n.next.and_then(|f| f.next);
+        }
+
+        slow
+    }
+}
+
