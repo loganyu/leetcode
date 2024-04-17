41ff89e7a1757ebee0122d5aaa67d5353b2f0a54
Logan Yu <yu.logan@gmail.com>
2024-04-17 00:01:19 +0000

    add 129_sum_root_to_leaf_numbers.java

---
+++ problems/129_sum_root_to_leaf_numbers.java
@@ -0,0 +1,96 @@
+/*
+ * You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+
+
+Example 1:
+
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+
+Input: root = [4,9,0,5,1]
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 9
+The depth of the tree will not exceed 10.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        int rootToLeaf = 0, currNumber = 0;
+        int steps;
+        TreeNode predecessor;
+        while (root != null) {
+            if (root.left != null) {
+                predecessor = root.left;
+                steps = 1;
+                while (predecessor.right != null && predecessor.right != root) {
+                predecessor = predecessor.right;
+                ++steps;
+                }
+                if (predecessor.right == null) {
+                    currNumber = currNumber * 10 + root.val;
+                    predecessor.right = root;
+                    root = root.left;
+                }
+                else {
+                if (predecessor.left == null) {
+                    rootToLeaf += currNumber;
+                }
+                for(int i = 0; i < steps; ++i) {
+                    currNumber /= 10;
+                }
+                predecessor.right = null;
+                root = root.right;
+                }
+            } else {
+                currNumber = currNumber * 10 + root.val;
+                // if you're on the leaf, update the sum
+                if (root.right == null) {
+                rootToLeaf += currNumber;
+                }
+                root = root.right;
+            }
+        }
+        return rootToLeaf;
+    }
+}
+
