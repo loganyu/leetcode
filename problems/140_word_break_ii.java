a27126141bc91dde33a5fa6d854ccc4ec5e98166
Logan Yu <yu.logan@gmail.com>
2024-06-01 02:24:21 +0000

    add 140_word_break_ii.java

---
+++ problems/140_word_break_ii.java
@@ -0,0 +1,67 @@
+/*
+ * Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+
+
+Example 1:
+
+Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+Output: ["cats and dog","cat sand dog"]
+Example 2:
+
+Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+Explanation: Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: []
+
+
+Constraints:
+
+1 <= s.length <= 20
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 10
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+Input is generated in a way that the length of the answer doesn't exceed 105.
+*/
+
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        List<String> results = new ArrayList<>();
+        backtrack(s, wordSet, new StringBuilder(), results, 0);
+        return results;
+    }
+
+    private void backtrack(
+        String s,
+        Set<String> wordSet,
+        StringBuilder currentSentence,
+        List<String> results,
+        int startIndex
+    ) {
+        if (startIndex == s.length()) {
+            results.add(currentSentence.toString().trim());
+            return;
+        }
+        for (
+            int endIndex = startIndex + 1;
+            endIndex <= s.length();
+            endIndex++
+        ) {
+            String word = s.substring(startIndex, endIndex);
+            if (wordSet.contains(word)) {
+                int currentLength = currentSentence.length();
+                currentSentence.append(word).append(" ");
+                backtrack(s, wordSet, currentSentence, results, endIndex);
+                currentSentence.setLength(currentLength);
+            }
+        }
+    }
+}
+
