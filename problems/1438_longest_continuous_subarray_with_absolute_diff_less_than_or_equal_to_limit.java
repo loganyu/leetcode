777194b215774eb39a42cc398c4919f76e400018
Logan Yu <yu.logan@gmail.com>
2024-06-25 03:03:25 +0000

    add 1438_longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.java

---
+++ problems/1438_longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.java
@@ -0,0 +1,72 @@
+/*
+ * Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
+
+
+
+Example 1:
+
+Input: nums = [8,2,4,7], limit = 4
+Output: 2
+Explanation: All subarrays are:
+[8] with maximum absolute diff |8-8| = 0 <= 4.
+[8,2] with maximum absolute diff |8-2| = 6 > 4.
+[8,2,4] with maximum absolute diff |8-2| = 6 > 4.
+[8,2,4,7] with maximum absolute diff |8-2| = 6 > 4.
+[2] with maximum absolute diff |2-2| = 0 <= 4.
+[2,4] with maximum absolute diff |2-4| = 2 <= 4.
+[2,4,7] with maximum absolute diff |2-7| = 5 > 4.
+[4] with maximum absolute diff |4-4| = 0 <= 4.
+[4,7] with maximum absolute diff |4-7| = 3 <= 4.
+[7] with maximum absolute diff |7-7| = 0 <= 4.
+Therefore, the size of the longest subarray is 2.
+Example 2:
+
+Input: nums = [10,1,2,4,7,2], limit = 5
+Output: 4
+Explanation: The subarray [2,4,7,2] is the longest since the maximum absolute diff is |2-7| = 5 <= 5.
+Example 3:
+
+Input: nums = [4,2,2,2,4,4,2,2], limit = 0
+Output: 3
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+0 <= limit <= 109
+*/
+
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+        Deque<Integer> maxDeque = new LinkedList<>();
+        Deque<Integer> minDeque = new LinkedList<>();
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < nums.length; ++right) {
+            while (!maxDeque.isEmpty() && maxDeque.peekLast() < nums[right]) {
+                maxDeque.pollLast();
+            }
+            maxDeque.offerLast(nums[right]);
+            while (!minDeque.isEmpty() && minDeque.peekLast() > nums[right]) {
+                minDeque.pollLast();
+            }
+            minDeque.offerLast(nums[right]);
+            while (maxDeque.peekFirst() - minDeque.peekFirst() > limit) {
+                if (maxDeque.peekFirst() == nums[left]) {
+                    maxDeque.pollFirst();
+                }
+                if (minDeque.peekFirst() == nums[left]) {
+                    minDeque.pollFirst();
+                }
+                ++left;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
+
