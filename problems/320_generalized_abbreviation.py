fcfa5dbe392d6f48ec1a76e61c1190d0edfb84a7
Logan Yu <yu.logan@gmail.com>
2024-08-29 02:54:54 +0000

    add 320_generalized_abbreviation.py

---
+++ problems/320_generalized_abbreviation.py
@@ -0,0 +1,58 @@
+'''
+A word's generalized abbreviation can be constructed by taking any number of non-overlapping and non-adjacent
+substrings
+ and replacing them with their respective lengths.
+
+For example, "abcde" can be abbreviated into:
+"a3e" ("bcd" turned into "3")
+"1bcd1" ("a" and "e" both turned into "1")
+"5" ("abcde" turned into "5")
+"abcde" (no substrings replaced)
+However, these abbreviations are invalid:
+"23" ("ab" turned into "2" and "cde" turned into "3") is invalid as the substrings chosen are adjacent.
+"22de" ("ab" turned into "2" and "bc" turned into "2") is invalid as the substring chosen overlap.
+Given a string word, return a list of all the possible generalized abbreviations of word. Return the answer in any order.
+
+
+
+Example 1:
+
+Input: word = "word"
+Output: ["4","3d","2r1","2rd","1o2","1o1d","1or1","1ord","w3","w2d","w1r1","w1rd","wo2","wo1d","wor1","word"]
+Example 2:
+
+Input: word = "a"
+Output: ["1","a"]
+
+
+Constraints:
+
+1 <= word.length <= 15
+word consists of only lowercase English letters.
+'''
+
+class Solution:
+    def generateAbbreviations(self, word: str) -> List[str]:
+        N = len(word)
+        abbreviations = []
+
+        for mask in range(1 << N):
+            curr_word = []
+            abbreviated_count = 0
+
+            for index in range(N):
+                if mask & (1 << index):
+                    abbreviated_count += 1
+                else:
+                    if abbreviated_count > 0:
+                        curr_word.append(str(abbreviated_count))
+                        abbreviated_count = 0
+                    curr_word.append(word[index])
+
+            if abbreviated_count > 0:
+                curr_word.append(str(abbreviated_count))
+
+            abbreviations.append("".join(curr_word))
+
+        return abbreviations
+
