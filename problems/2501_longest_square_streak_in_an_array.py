de04aa08088c754c03a4f645c69394ec90a9348b
Logan Yu <yu.logan@gmail.com>
2024-10-29 01:15:26 +0000

    add 2501_longest_square_streak_in_an_array.py

---
+++ problems/2501_longest_square_streak_in_an_array.py
@@ -0,0 +1,49 @@
+'''
+You are given an integer array nums. A subsequence of nums is called a square streak if:
+
+The length of the subsequence is at least 2, and
+after sorting the subsequence, each element (except the first element) is the square of the previous number.
+Return the length of the longest square streak in nums, or return -1 if there is no square streak.
+
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+
+
+Example 1:
+
+Input: nums = [4,3,6,16,8,2]
+Output: 3
+Explanation: Choose the subsequence [4,16,2]. After sorting it, it becomes [2,4,16].
+- 4 = 2 * 2.
+- 16 = 4 * 4.
+Therefore, [4,16,2] is a square streak.
+It can be shown that every subsequence of length 4 is not a square streak.
+Example 2:
+
+Input: nums = [2,3,5,6,7]
+Output: -1
+Explanation: There is no square streak in nums so return -1.
+
+
+Constraints:
+
+2 <= nums.length <= 105
+2 <= nums[i] <= 105
+'''
+
+class Solution:
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        longest_streak = 0
+        unique_numbers = set(nums)
+        for start_number in nums:
+            current_streak = 0
+            current = start_number
+            while current in unique_numbers:
+                current_streak += 1
+                if current * current > 10**5:
+                    break
+                current *= current
+            longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak if longest_streak >= 2 else -1
+
