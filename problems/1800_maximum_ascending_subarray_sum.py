f1de919608dace466d8352a39c52c3f4bf3bbf28
Logan Yu <yu.logan@gmail.com>
2025-02-09 02:36:52 +0000

    add 1800_maximum_ascending_subarray_sum.py

---
+++ problems/1800_maximum_ascending_subarray_sum.py
@@ -0,0 +1,44 @@
+'''
+Given an array of positive integers nums, return the maximum possible sum of an ascending subarray in nums.
+
+A subarray is defined as a contiguous sequence of numbers in an array.
+
+A subarray [numsl, numsl+1, ..., numsr-1, numsr] is ascending if for all i where l <= i < r, numsi  < numsi+1. Note that a subarray of size 1 is ascending.
+
+
+
+Example 1:
+
+Input: nums = [10,20,30,5,10,50]
+Output: 65
+Explanation: [5,10,50] is the ascending subarray with the maximum sum of 65.
+Example 2:
+
+Input: nums = [10,20,30,40,50]
+Output: 150
+Explanation: [10,20,30,40,50] is the ascending subarray with the maximum sum of 150.
+Example 3:
+
+Input: nums = [12,17,15,13,10,11,12]
+Output: 33
+Explanation: [10,11,12] is the ascending subarray with the maximum sum of 33.
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+'''
+
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        maxSum = 0
+        currentSubarraySum = nums[0]
+        for currentIdx in range(1, len(nums)):
+            if nums[currentIdx] <= nums[currentIdx - 1]:
+                maxSum = max(maxSum, currentSubarraySum)
+                currentSubarraySum = 0
+            currentSubarraySum += nums[currentIdx]
+
+        return max(maxSum, currentSubarraySum)
+
