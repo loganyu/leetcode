23cd0775c701f70f84894c6cf3354281530be011
Logan Yu <yu.logan@gmail.com>
2025-03-19 00:25:22 +0000

    add 3174_clear_digits.py

---
+++ problems/3174_clear_digits.py
@@ -0,0 +1,55 @@
+'''
+You are given a string s.
+
+Your task is to remove all digits by doing this operation repeatedly:
+
+Delete the first digit and the closest non-digit character to its left.
+Return the resulting string after removing all digits.
+
+
+
+Example 1:
+
+Input: s = "abc"
+
+Output: "abc"
+
+Explanation:
+
+There is no digit in the string.
+
+Example 2:
+
+Input: s = "cb34"
+
+Output: ""
+
+Explanation:
+
+First, we apply the operation on s[2], and s becomes "c4".
+
+Then we apply the operation on s[1], and s becomes "".
+
+
+
+Constraints:
+
+1 <= s.length <= 100
+s consists only of lowercase English letters and digits.
+The input is generated such that it is possible to delete all digits.
+'''
+
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        answer_length = 0
+        s = list(s)
+        for char_index in range(len(s)):
+            if s[char_index].isdigit():
+                answer_length = max(answer_length - 1, 0)
+            else:
+                s[answer_length] = s[char_index]
+                answer_length += 1
+        s = s[:answer_length]
+
+        return "".join(s)
+
