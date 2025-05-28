f5ebfe36643030b937150878d9798310dfcd81fb
Logan Yu <yu.logan@gmail.com>
2025-05-28 03:17:05 +0000

    add 912_sort_an_array.java

---
+++ problems/912_sort_an_array.java
@@ -0,0 +1,53 @@
+/*
+ * Given an array of integers nums, sort the array in ascending order and return it.
+
+You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+Example 2:
+
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+Explanation: Note that the values of nums are not necessairly unique.
+
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+-5 * 104 <= nums[i] <= 5 * 104
+
+*/
+
+class Solution {
+    private void countingSort(int[] arr) {
+        HashMap<Integer,Integer> counts = new HashMap<>();
+        int minVal = arr[0], maxVal = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            minVal = Math.min(minVal, arr[i]);
+            maxVal = Math.max(maxVal, arr[i]);
+            counts.put(arr[i], counts.getOrDefault(arr[i], 0) + 1);
+        }
+
+        int index = 0;
+        for (int val = minVal; val <= maxVal; ++val) {
+            while (counts.getOrDefault(val, 0) > 0) {
+                arr[index] = val;
+                index += 1;
+                counts.put(val, counts.get(val) - 1);
+            }
+        }
+    }
+
+    public int[] sortArray(int[] nums) {
+        countingSort(nums);
+        return nums;
+    }
+}
+
