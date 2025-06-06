0ff78234e39181bd0fa442ea05600ac8e1df02bc
Logan Yu <yu.logan@gmail.com>
2025-06-06 03:25:52 +0000

    add 912_sort_an_array.py

---
+++ problems/912_sort_an_array.py
@@ -0,0 +1,44 @@
+'''
+Given an array of integers nums, sort the array in ascending order and return it.
+
+You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+Example 2:
+
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+Explanation: Note that the values of nums are not necessairly unique.
+
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+-5 * 104 <= nums[i] <= 5 * 104
+'''
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def counting_sort():
+            counts = {}
+            minVal, maxVal = min(nums), max(nums)
+            for val in nums:
+                counts[val] = counts.get(val, 0) + 1
+
+            index = 0
+            for val in range(minVal, maxVal + 1, 1):
+                while counts.get(val, 0) > 0:
+                    nums[index] = val
+                    index += 1
+                    counts[val] -= 1
+
+        counting_sort()
+
+        return nums
+
