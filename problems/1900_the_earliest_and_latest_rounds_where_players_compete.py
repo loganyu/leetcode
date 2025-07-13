6d6f5902eee97a801131aa12602d5d2acfdf8631
Logan Yu <yu.logan@gmail.com>
2025-07-13 00:04:58 +0000

    add 1900_the_earliest_and_latest_rounds_where_players_compete.py

---
+++ problems/1900_the_earliest_and_latest_rounds_where_players_compete.py
@@ -0,0 +1,82 @@
+'''
+There is a tournament where n players are participating. The players are standing in a single row and are numbered from 1 to n based on their initial standing position (player 1 is the first player in the row, player 2 is the second player in the row, etc.).
+
+The tournament consists of multiple rounds (starting from round number 1). In each round, the ith player from the front of the row competes against the ith player from the end of the row, and the winner advances to the next round. When the number of players is odd for the current round, the player in the middle automatically advances to the next round.
+
+For example, if the row consists of players 1, 2, 4, 6, 7
+Player 1 competes against player 7.
+Player 2 competes against player 6.
+Player 4 automatically advances to the next round.
+After each round is over, the winners are lined back up in the row based on the original ordering assigned to them initially (ascending order).
+
+The players numbered firstPlayer and secondPlayer are the best in the tournament. They can win against any other player before they compete against each other. If any two other players compete against each other, either of them might win, and thus you may choose the outcome of this round.
+
+Given the integers n, firstPlayer, and secondPlayer, return an integer array containing two values, the earliest possible round number and the latest possible round number in which these two players will compete against each other, respectively.
+
+
+
+Example 1:
+
+Input: n = 11, firstPlayer = 2, secondPlayer = 4
+Output: [3,4]
+Explanation:
+One possible scenario which leads to the earliest round number:
+First round: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+Second round: 2, 3, 4, 5, 6, 11
+Third round: 2, 3, 4
+One possible scenario which leads to the latest round number:
+First round: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+Second round: 1, 2, 3, 4, 5, 6
+Third round: 1, 2, 4
+Fourth round: 2, 4
+Example 2:
+
+Input: n = 5, firstPlayer = 1, secondPlayer = 5
+Output: [1,1]
+Explanation: The players numbered 1 and 5 compete in the first round.
+There is no way to make them compete in any other round.
+
+
+Constraints:
+
+2 <= n <= 28
+1 <= firstPlayer < secondPlayer <= n
+'''
+
+class Solution:
+    def earliestAndLatest(self, n: int, firstPlayer: int, secondPlayer: int) -> List[int]:
+        @cache
+        def dp(n: int, f: int, s: int) -> (int, int):
+            if f + s == n + 1:
+                return (1, 1)
+
+            if f + s > n + 1:
+                return dp(n, n + 1 - s, n + 1 - f)
+
+            earliest, latest = float("inf"), float("-inf")
+            n_half = (n + 1) // 2
+
+            if s <= n_half:
+                for i in range(f):
+                    for j in range(s - f):
+                        x, y = dp(n_half, i + 1, i + j + 2)
+                        earliest = min(earliest, x)
+                        latest = max(latest, y)
+            else:
+                s_prime = n + 1 - s
+                mid = (n - 2 * s_prime + 1) // 2
+                for i in range(f):
+                    for j in range(s_prime - f):
+                        x, y = dp(n_half, i + 1, i + j + mid + 2)
+                        earliest = min(earliest, x)
+                        latest = max(latest, y)
+
+            return (earliest + 1, latest + 1)
+
+        if firstPlayer > secondPlayer:
+            firstPlayer, secondPlayer = secondPlayer, firstPlayer
+
+        earliest, latest = dp(n, firstPlayer, secondPlayer)
+        dp.cache_clear()
+        return [earliest, latest]
+
