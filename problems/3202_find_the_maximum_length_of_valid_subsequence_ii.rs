34f8063c099094cecf4d84a77607b1f7457d60e8
Logan Yu <yu.logan@gmail.com>
2025-07-20 03:15:53 +0000

    add 3202_find_the_maximum_length_of_valid_subsequence_ii.rs

---
+++ problems/3202_find_the_maximum_length_of_valid_subsequence_ii.rs
@@ -0,0 +1,53 @@
+/*
+ * You are given an integer array nums and a positive integer k.
+A subsequence sub of nums with length x is called valid if it satisfies:
+
+(sub[0] + sub[1]) % k == (sub[1] + sub[2]) % k == ... == (sub[x - 2] + sub[x - 1]) % k.
+Return the length of the longest valid subsequence of nums.
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5], k = 2
+
+Output: 5
+
+Explanation:
+
+The longest valid subsequence is [1, 2, 3, 4, 5].
+
+Example 2:
+
+Input: nums = [1,4,2,3,1,4], k = 3
+
+Output: 4
+
+Explanation:
+
+The longest valid subsequence is [1, 4, 1, 4].
+
+
+
+Constraints:
+
+2 <= nums.length <= 103
+1 <= nums[i] <= 107
+1 <= k <= 103
+*/
+
+impl Solution {
+    pub fn maximum_length(nums: Vec<i32>, k: i32) -> i32 {
+        let k = k as usize;
+        let mut dp = vec![vec![0; k]; k];
+        let mut res = 0;
+        for num in nums {
+            let mod_num = (num % k as i32) as usize;
+            for prev in 0..k {
+                dp[prev][mod_num] = dp[mod_num][prev] + 1;
+                res = res.max(dp[prev][mod_num]);
+            }
+        }
+        res
+    }
+}
+
