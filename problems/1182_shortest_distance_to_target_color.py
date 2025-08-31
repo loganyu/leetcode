786d1bef0d3da96fc2a55fc8afea46fe2f5a0952
Logan Yu <yu.logan@gmail.com>
2025-08-31 17:27:53 +0000

    add 1182_shortest_distance_to_target_color.py

---
+++ problems/1182_shortest_distance_to_target_color.py
@@ -0,0 +1,53 @@
+'''
+You are given an array colors, in which there are three colors: 1, 2 and 3.
+
+You are also given some queries. Each query consists of two integers i and c, return the shortest distance between the given index i and the target color c. If there is no solution return -1.
+
+
+
+Example 1:
+
+Input: colors = [1,1,2,1,3,2,2,3,3], queries = [[1,3],[2,2],[6,1]]
+Output: [3,0,3]
+Explanation:
+The nearest 3 from index 1 is at index 4 (3 steps away).
+The nearest 2 from index 2 is at index 2 itself (0 steps away).
+The nearest 1 from index 6 is at index 3 (3 steps away).
+Example 2:
+
+Input: colors = [1,2], queries = [[0,3]]
+Output: [-1]
+Explanation: There is no 3 in the array.
+
+
+Constraints:
+
+1 <= colors.length <= 5*10^4
+1 <= colors[i] <= 3
+1 <= queries.length <= 5*10^4
+queries[i].length == 2
+0 <= queries[i][0] < colors.length
+1 <= queries[i][1] <= 3
+'''
+
+class Solution:
+    def shortestDistanceColor(self, colors: List[int], queries: List[List[int]]) -> List[int]:
+        n = len(colors)
+        rightmost = [0, 0, 0]
+        leftmost = [n - 1, n - 1, n - 1]
+
+        distance = [[-1] * n for _ in range(3)]
+        for i in range(n):
+            color = colors[i] - 1
+            for j in range(rightmost[color], i + 1):
+                distance[color][j] = i - j
+            rightmost[color] = i + 1
+        for i in range(n - 1, -1, -1):
+            color = colors[i] - 1
+            for j in range(leftmost[color], i - 1, -1):
+                if distance[color][j] == -1 or distance[color][j] > j - i:
+                    distance[color][j] = j - i
+            leftmost[color] = i - 1
+
+        return [distance[color - 1][index] for index,color in queries]
+
