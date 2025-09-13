3867e54542e39abbf5fdff58f6000a6fc76db1c8
Logan Yu <yu.logan@gmail.com>
2025-09-13 02:02:50 +0000

    add 1317_convert_integer_to_the_sum_of_two_no_zero_integers.rs

---
+++ problems/1317_convert_integer_to_the_sum_of_two_no_zero_integers.rs
@@ -0,0 +1,44 @@
+/*
+ * No-Zero integer is a positive integer that does not contain any 0 in its decimal representation.
+
+Given an integer n, return a list of two integers [a, b] where:
+
+a and b are No-Zero integers.
+a + b = n
+The test cases are generated so that there is at least one valid solution. If there are many valid solutions, you can return any of them.
+
+
+
+Example 1:
+
+Input: n = 2
+Output: [1,1]
+Explanation: Let a = 1 and b = 1.
+Both a and b are no-zero integers, and a + b = 2 = n.
+Example 2:
+
+Input: n = 11
+Output: [2,9]
+Explanation: Let a = 2 and b = 9.
+Both a and b are no-zero integers, and a + b = 11 = n.
+Note that there are other valid answers as [8, 3] that can be accepted.
+
+
+Constraints:
+
+2 <= n <= 104
+*/
+
+impl Solution {
+    pub fn get_no_zero_integers(n: i32) -> Vec<i32> {
+        for a in 1..n {
+            let b = n - a;
+            if !a.to_string().contains('0') && !b.to_string().contains('0') {
+                return vec![a, b];
+            }
+        }
+
+        unreachable!();
+    }
+}
+
