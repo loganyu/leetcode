a634e846f6c880f162b5ee39cf117e2bccfacca5
Logan Yu <yu.logan@gmail.com>
2026-01-29 02:14:01 +0000

    add 3507_minimum_pair_removal_to_sort_array_i.py

---
+++ problems/3507_minimum_pair_removal_to_sort_array_i.py
@@ -0,0 +1,56 @@
+'''
+Given an array nums, you can perform the following operation any number of times:
+
+Select the adjacent pair with the minimum sum in nums. If multiple such pairs exist, choose the leftmost one.
+Replace the pair with their sum.
+Return the minimum number of operations needed to make the array non-decreasing.
+
+An array is said to be non-decreasing if each element is greater than or equal to its previous element (if it exists).
+
+
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+
+Output: 2
+
+Explanation:
+
+The pair (3,1) has the minimum sum of 4. After replacement, nums = [5,2,4].
+The pair (2,4) has the minimum sum of 6. After replacement, nums = [5,6].
+The array nums became non-decreasing in two operations.
+
+Example 2:
+
+Input: nums = [1,2,2]
+
+Output: 0
+
+Explanation:
+
+The array nums is already sorted.
+
+
+
+Constraints:
+
+1 <= nums.length <= 50
+-1000 <= nums[i] <= 1000
+'''
+
+class Solution:
+    def minimumPairRemoval(self, nums: List[int]) -> int:
+        c = 0
+        while nums != sorted(nums):
+            min_val =[]
+            m =float('inf')
+            for i in range(1,len(nums)):
+                min_val.append(nums[i-1]+nums[i])
+            m = min(min_val)
+            n = min_val.index(m)
+            nums[n] = m
+            nums.pop(n+1)
+            c += 1
+        return c
+
