1d88e5b0843d676a73247877b0a6d7ed78c371c4
Logan Yu <yu.logan@gmail.com>
2026-01-30 06:36:35 +0000

    add 3531_count_covered_buildings.py

---
+++ problems/3531_count_covered_buildings.py
@@ -0,0 +1,90 @@
+'''
+You are given a positive integer n, representing an n x n city. You are also given a 2D grid buildings, where buildings[i] = [x, y] denotes a unique building located at coordinates [x, y].
+
+A building is covered if there is at least one building in all four directions: left, right, above, and below.
+
+Return the number of covered buildings.
+
+
+
+Example 1:
+
+
+
+Input: n = 3, buildings = [[1,2],[2,2],[3,2],[2,1],[2,3]]
+
+Output: 1
+
+Explanation:
+
+Only building [2,2] is covered as it has at least one building:
+above ([1,2])
+below ([3,2])
+left ([2,1])
+right ([2,3])
+Thus, the count of covered buildings is 1.
+Example 2:
+
+
+
+Input: n = 3, buildings = [[1,1],[1,2],[2,1],[2,2]]
+
+Output: 0
+
+Explanation:
+
+No building has at least one building in all four directions.
+Example 3:
+
+
+
+Input: n = 5, buildings = [[1,3],[3,2],[3,3],[3,5],[5,3]]
+
+Output: 1
+
+Explanation:
+
+Only building [3,3] is covered as it has at least one building:
+above ([1,3])
+below ([5,3])
+left ([3,2])
+right ([3,5])
+Thus, the count of covered buildings is 1.
+
+
+Constraints:
+
+2 <= n <= 105
+1 <= buildings.length <= 105
+buildings[i] = [x, y]
+1 <= x, y <= n
+All coordinates of buildings are unique.
+'''
+
+class Solution:
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
+        max_row = [0] * (n + 1)
+        min_row = [n + 1] * (n + 1)
+        max_col = [0] * (n + 1)
+        min_col = [n + 1] * (n + 1)
+
+        for p in buildings:
+            x, y = p[0], p[1]
+            max_row[y] = max(max_row[y], x)
+            min_row[y] = min(min_row[y], x)
+            max_col[x] = max(max_col[x], y)
+            min_col[x] = min(min_col[x], y)
+
+        res = 0
+        for p in buildings:
+            x, y = p[0], p[1]
+            if (
+                x > min_row[y]
+                and x < max_row[y]
+                and y > min_col[x]
+                and y < max_col[x]
+            ):
+                res += 1
+
+        return res
+
