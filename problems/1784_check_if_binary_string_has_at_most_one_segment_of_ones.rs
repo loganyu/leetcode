0ccfdcae1dc6378bff40bab24088d22191bd41ca
Logan Yu <yu.logan@gmail.com>
2026-03-07 02:40:10 +0000

    add 1784_check_if_binary_string_has_at_most_one_segment_of_ones.rs

---
+++ problems/1784_check_if_binary_string_has_at_most_one_segment_of_ones.rs
@@ -0,0 +1,29 @@
+/*
+ * Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
+
+
+
+Example 1:
+
+Input: s = "1001"
+Output: false
+Explanation: The ones do not form a contiguous segment.
+Example 2:
+
+Input: s = "110"
+Output: true
+
+
+Constraints:
+
+1 <= s.length <= 100
+s[i]​​​​ is either '0' or '1'.
+s[0] is '1'.
+*/
+
+impl Solution {
+    pub fn check_ones_segment(s: String) -> bool {
+        !s.contains("01")
+    }
+}
+
