1e25d9a5903871407f504bae1b356a0527e5221c
Logan Yu <yu.logan@gmail.com>
2026-03-23 20:22:35 +0000

    add 1886_determine_whether_matrix_can_be_obtained_by_rotation.py

---
+++ problems/1886_determine_whether_matrix_can_be_obtained_by_rotation.py
@@ -0,0 +1,55 @@
+'''
+Given two n x n binary matrices mat and target, return true if it is possible to make mat equal to target by rotating mat in 90-degree increments, or false otherwise.
+
+
+
+Example 1:
+
+
+Input: mat = [[0,1],[1,0]], target = [[1,0],[0,1]]
+Output: true
+Explanation: We can rotate mat 90 degrees clockwise to make mat equal target.
+Example 2:
+
+
+Input: mat = [[0,1],[1,1]], target = [[1,0],[0,1]]
+Output: false
+Explanation: It is impossible to make mat equal to target by rotating mat.
+Example 3:
+
+
+Input: mat = [[0,0,0],[0,1,0],[1,1,1]], target = [[1,1,1],[0,1,0],[0,0,0]]
+Output: true
+Explanation: We can rotate mat 90 degrees clockwise two times to make mat equal target.
+
+
+Constraints:
+
+n == mat.length == target.length
+n == mat[i].length == target[i].length
+1 <= n <= 10
+mat[i][j] and target[i][j] are either 0 or 1.
+'''
+
+class Solution:
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        n = len(mat)
+        for k in range(4):
+            for i in range(n // 2):
+                for j in range((n + 1) // 2):
+                    (
+                        mat[i][j],
+                        mat[n - 1 - j][i],
+                        mat[n - 1 - i][n - 1 - j],
+                        mat[j][n - 1 - i],
+                    ) = (
+                        mat[n - 1 - j][i],
+                        mat[n - 1 - i][n - 1 - j],
+                        mat[j][n - 1 - i],
+                        mat[i][j],
+                    )
+
+            if mat == target:
+                return True
+        return False
+
