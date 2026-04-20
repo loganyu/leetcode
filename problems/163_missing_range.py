cdf6a59eedc176f6b2496c58c69c2f3cc8f4cb02
Logan Yu <yu.logan@gmail.com>
2026-04-20 00:27:25 +0000

    fix 163_missing_range.py

--- problems/163_missing_range.py
+++ problems/163_missing_range.py
@@ -9,16 +9,21 @@ Output: ["2", "4->49", "51->74", "76->99"]
 
 class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
-        ranges = []
-        prev = lower - 1
-        nums.append(upper + 1)
-        for num in nums:
-            if num == prev + 2:
-                ranges.append(f"{num-1}")
-            elif num > prev + 2:
-                ranges.append(f"{prev+1}->{num-1}")
-            
-            prev = num
-        
-        return ranges
-    
+        n = len(nums)
+        missing_ranges = []
+        if n == 0:
+            missing_ranges.append([lower, upper])
+            return missing_ranges
+
+        if lower < nums[0]:
+            missing_ranges.append([lower, nums[0] - 1])
+
+        for i in range(n - 1):
+            if nums[i + 1] - nums[i] <= 1:
+                continue
+            missing_ranges.append([nums[i] + 1, nums[i + 1] - 1])
+
+        if upper > nums[n - 1]:
+            missing_ranges.append([nums[n - 1] + 1, upper])
+
+        return missing_ranges
