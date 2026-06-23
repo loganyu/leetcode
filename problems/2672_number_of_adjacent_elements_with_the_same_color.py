d930892663d9e7b081b850cf957808b231d6e157
Logan Yu <yu.logan@gmail.com>
2026-06-23 13:45:47 +0000

    add 2672_number_of_adjacent_elements_with_the_same_color.py

---
+++ problems/2672_number_of_adjacent_elements_with_the_same_color.py
@@ -0,0 +1,67 @@
+'''
+You are given an integer n representing an array colors of length n where all elements are set to 0's meaning uncolored. You are also given a 2D integer array queries where queries[i] = [indexi, colori]. For the ith query:
+
+Set colors[indexi] to colori.
+Count the number of adjacent pairs in colors which have the same color (regardless of colori).
+Return an array answer of the same length as queries where answer[i] is the answer to the ith query.
+
+
+
+Example 1:
+
+Input: n = 4, queries = [[0,2],[1,2],[3,1],[1,1],[2,1]]
+
+Output: [0,1,1,0,2]
+
+Explanation:
+
+Initially array colors = [0,0,0,0], where 0 denotes uncolored elements of the array.
+After the 1st query colors = [2,0,0,0]. The count of adjacent pairs with the same color is 0.
+After the 2nd query colors = [2,2,0,0]. The count of adjacent pairs with the same color is 1.
+After the 3rd query colors = [2,2,0,1]. The count of adjacent pairs with the same color is 1.
+After the 4th query colors = [2,1,0,1]. The count of adjacent pairs with the same color is 0.
+After the 5th query colors = [2,1,1,1]. The count of adjacent pairs with the same color is 2.
+Example 2:
+
+Input: n = 1, queries = [[0,100000]]
+
+Output: [0]
+
+Explanation:
+
+After the 1st query colors = [100000]. The count of adjacent pairs with the same color is 0.
+
+
+
+Constraints:
+
+1 <= n <= 105
+1 <= queries.length <= 105
+queries[i].length == 2
+0 <= indexi <= n - 1
+1 <=  colori <= 105
+'''
+
+class Solution:
+    def colorTheArray(self, n: int, queries: List[List[int]]) -> List[int]:
+        colors = [0] * n
+        pairs = 0
+        answer = []
+
+        for idx, color in queries:
+            if colors[idx] != 0:
+                if idx > 0 and colors[idx] == colors[idx - 1]:
+                    pairs -= 1
+                if idx < n - 1 and colors[idx] == colors[idx + 1]:
+                    pairs -= 1
+
+            colors[idx] = color
+
+            if idx > 0 and colors[idx] == colors[idx - 1]:
+                pairs += 1
+            if idx < n - 1 and colors[idx] == colors[idx + 1]:
+                pairs += 1
+
+            answer.append(pairs)
+
+        return answer
