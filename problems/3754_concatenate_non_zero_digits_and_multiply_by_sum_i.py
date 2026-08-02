2fcc8cde811d6ba887a4aed0ae4ec7ff94dd13bc
Logan Yu <yu.logan@gmail.com>
2026-08-02 17:04:39 +0000

    add 3754_concatenate_non_zero_digits_and_multiply_by_sum_i.py

---
+++ problems/3754_concatenate_non_zero_digits_and_multiply_by_sum_i.py
@@ -0,0 +1,58 @@
+'''
+You are given an integer n.
+
+Form a new integer x by concatenating all the non-zero digits of n in their original order. If there are no non-zero digits, x = 0.
+
+Let sum be the sum of digits in x.
+
+Return an integer representing the value of x * sum.
+
+
+
+Example 1:
+
+Input: n = 10203004
+
+Output: 12340
+
+Explanation:
+
+The non-zero digits are 1, 2, 3, and 4. Thus, x = 1234.
+The sum of digits is sum = 1 + 2 + 3 + 4 = 10.
+Therefore, the answer is x * sum = 1234 * 10 = 12340.
+Example 2:
+
+Input: n = 1000
+
+Output: 1
+
+Explanation:
+
+The non-zero digit is 1, so x = 1 and sum = 1.
+Therefore, the answer is x * sum = 1 * 1 = 1.
+
+
+Constraints:
+
+0 <= n <= 109
+'''
+
+class Solution:
+    def sumAndMultiply(self, n: int) -> int:
+        n_str = str(n)
+
+        filtered_digits = []
+        digit_sum = 0
+
+        for char in n_str:
+            if char != '0':
+                filtered_digits.append(char)
+                digit_sum += int(char)
+
+        if not filtered_digits:
+            return 0
+
+        x = int("".join(filtered_digits))
+
+        return x * digit_sum
+
